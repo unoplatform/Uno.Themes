82260c3911591ac3b42369dc93ed98eb8b4d8777
Mathieu Fillion <mathieu.fillion@nventive.com>
2020-04-29 17:34:33 -0400

    Flatten resource dictionaries

--- src/library/Uno.Material/MaterialLibraryResources.cs
+++ src/library/Uno.Material/MaterialLibraryResources.cs
@@ -3,11 +3,22 @@ using Windows.UI.Xaml;
 
 namespace Uno.Material
 {
+	/// <summary>
+	/// Material resources including colors, layout values and styles
+	/// </summary>
 	public sealed class MaterialLibraryResources : ResourceDictionary
 	{
 		public MaterialLibraryResources()
 		{
-			this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///Uno.Material/Styles/_Styles.xaml") });
+			// Add all ResourceDictionaries here in alphabetical order
+			this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///Uno.Material/Styles/Application/Colors.xaml") });
+
+			this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///Uno.Material/Styles/Controls/Button.xaml") });
+			this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///Uno.Material/Styles/Controls/Card.xaml") });
+			this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///Uno.Material/Styles/Controls/CheckBox.xaml") });
+			this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///Uno.Material/Styles/Controls/CommandBar.xaml") });
+			this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///Uno.Material/Styles/Controls/RadioButton.xaml") });
+			this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///Uno.Material/Styles/Controls/TextBox.xaml") });
 		}
 	}
 }
