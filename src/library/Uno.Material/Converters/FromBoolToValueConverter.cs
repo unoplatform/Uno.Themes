bdd8cdc264b2fa9b5e3c1e807f910f0ef8f43579
Agnès Zitte <agnes.zitte@nventive.com>
2020-05-21 14:03:21 -0400

    feat(card): Added CardControl

---
+++ src/library/Uno.Material/Converters/FromBoolToValueConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Data;
+
+namespace Uno.Material.Converters
+{
+	public class FromBoolToValueConverter : IValueConverter
+	{
+		public object NullValue { get; set; }
+
+		public object FalseValue { get; set; }
+
+		public object TrueValue { get; set; }
+
+		public object NullOrFalseValue
+		{
+			get { return FalseValue; }
+			set { FalseValue = NullValue = value; }
+		}
+
+		public object NullOrTrueValue
+		{
+			get { return TrueValue; }
+			set { TrueValue = NullValue = value; }
+		}
+
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			if (value == null)
+			{
+				return NullValue;
+			}
+
+			if (System.Convert.ToBoolean(value))
+			{
+				return TrueValue;
+			}
+			else
+			{
+				return FalseValue;
+			}
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
