b02746cb5085eb5b950cde7fa16a51388316a4a5
Agnès Zitte <agnes.zitte@nventive.com>
2020-06-12 10:22:40 -0400

    feat(card): Properly rearrange Card control templates and added elevated styles

---
+++ src/library/Uno.Material/Controls/Card.Properties.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Uno.Material.Controls
+{
+	public partial class Card : Control
+	{
+		#region HeaderContent and HeaderContentTemplate
+		public object HeaderContent
+		{
+			get { return (object)GetValue(HeaderContentProperty); }
+			set { SetValue(HeaderContentProperty, value); }
+		}
+
+		public static readonly DependencyProperty HeaderContentProperty =
+			DependencyProperty.Register("HeaderContent", typeof(object), typeof(Card), new PropertyMetadata(null));
+
+		public DataTemplate HeaderContentTemplate
+		{
+			get { return (DataTemplate)GetValue(HeaderContentTemplateProperty); }
+			set { SetValue(HeaderContentTemplateProperty, value); }
+		}
+
+		public static readonly DependencyProperty HeaderContentTemplateProperty =
+			DependencyProperty.Register("HeaderContentTemplate", typeof(DataTemplate), typeof(Card), new PropertyMetadata(null));
+		#endregion
+
+		#region SubHeaderContent and SubHeaderContentTemplate
+		public object SubHeaderContent
+		{
+			get { return (object)GetValue(SubHeaderContentProperty); }
+			set { SetValue(SubHeaderContentProperty, value); }
+		}
+
+		public static readonly DependencyProperty SubHeaderContentProperty =
+			DependencyProperty.Register("SubHeaderContent", typeof(object), typeof(Card), new PropertyMetadata(null));
+
+		public DataTemplate SubHeaderContentTemplate
+		{
+			get { return (DataTemplate)GetValue(SubHeaderContentTemplateProperty); }
+			set { SetValue(SubHeaderContentTemplateProperty, value); }
+		}
+
+		public static readonly DependencyProperty SubHeaderContentTemplateProperty =
+			DependencyProperty.Register("SubHeaderContentTemplate", typeof(DataTemplate), typeof(Card), new PropertyMetadata(null));
+		#endregion
+
+		#region AvatarContent and AvatarContentTemplate
+		public object AvatarContent
+		{
+			get { return (object)GetValue(AvatarContentProperty); }
+			set { SetValue(AvatarContentProperty, value); }
+		}
+
+		public static readonly DependencyProperty AvatarContentProperty =
+			DependencyProperty.Register("AvatarContent", typeof(object), typeof(Card), new PropertyMetadata(null));
+
+		public DataTemplate AvatarContentTemplate
+		{
+			get { return (DataTemplate)GetValue(AvatarContentTemplateProperty); }
+			set { SetValue(AvatarContentTemplateProperty, value); }
+		}
+
+		public static readonly DependencyProperty AvatarContentTemplateProperty =
+			DependencyProperty.Register("AvatarContentTemplate", typeof(DataTemplate), typeof(Card), new PropertyMetadata(null));
+		#endregion
+
+		#region MediaContent and MediaContentTemplate
+		public object MediaContent
+		{
+			get { return (object)GetValue(MediaContentProperty); }
+			set { SetValue(MediaContentProperty, value); }
+		}
+
+		public static readonly DependencyProperty MediaContentProperty =
+			DependencyProperty.Register("MediaContent", typeof(object), typeof(Card), new PropertyMetadata(null));
+
+		public DataTemplate MediaContentTemplate
+		{
+			get { return (DataTemplate)GetValue(MediaContentTemplateProperty); }
+			set { SetValue(MediaContentTemplateProperty, value); }
+		}
+
+		public static readonly DependencyProperty MediaContentTemplateProperty =
+			DependencyProperty.Register("MediaContentTemplate", typeof(DataTemplate), typeof(Card), new PropertyMetadata(null));
+		#endregion
+
+		#region SupportingContent and SupportingContentTemplate
+		public object SupportingContent
+		{
+			get { return (object)GetValue(SupportingContentProperty); }
+			set { SetValue(SupportingContentProperty, value); }
+		}
+
+		public static readonly DependencyProperty SupportingContentProperty =
+			DependencyProperty.Register("SupportingContent", typeof(object), typeof(Card), new PropertyMetadata(null));
+
+		public DataTemplate SupportingContentTemplate
+		{
+			get { return (DataTemplate)GetValue(SupportingContentTemplateProperty); }
+			set { SetValue(SupportingContentTemplateProperty, value); }
+		}
+
+		public static readonly DependencyProperty SupportingContentTemplateProperty =
+			DependencyProperty.Register("SupportingContentTemplate", typeof(DataTemplate), typeof(Card), new PropertyMetadata(null));
+		#endregion
+
+		#region IconsContent and IconsContentTemplate
+		public object IconsContent
+		{
+			get { return (object)GetValue(IconsContentProperty); }
+			set { SetValue(IconsContentProperty, value); }
+		}
+
+		public static readonly DependencyProperty IconsContentProperty =
+			DependencyProperty.Register("IconsContent", typeof(object), typeof(Card), new PropertyMetadata(null));
+
+		public DataTemplate IconsContentTemplate
+		{
+			get { return (DataTemplate)GetValue(IconsContentTemplateProperty); }
+			set { SetValue(IconsContentTemplateProperty, value); }
+		}
+
+		public static readonly DependencyProperty IconsContentTemplateProperty =
+			DependencyProperty.Register("IconsContentTemplate", typeof(DataTemplate), typeof(Card), new PropertyMetadata(null));
+		#endregion
+
+		#region ExpanderHeaderContent and ExpanderHeaderContentTemplate
+		public object ExpanderHeaderContent
+		{
+			get { return (object)GetValue(ExpanderHeaderContentProperty); }
+			set { SetValue(ExpanderHeaderContentProperty, value); }
+		}
+
+		public static readonly DependencyProperty ExpanderHeaderContentProperty =
+			DependencyProperty.Register("ExpanderHeaderContent", typeof(object), typeof(Card), new PropertyMetadata(null));
+
+		public DataTemplate ExpanderHeaderContentTemplate
+		{
+			get { return (DataTemplate)GetValue(ExpanderHeaderContentTemplateProperty); }
+			set { SetValue(ExpanderHeaderContentTemplateProperty, value); }
+		}
+
+		public static readonly DependencyProperty ExpanderHeaderContentTemplateProperty =
+			DependencyProperty.Register("ExpanderHeaderContentTemplate", typeof(DataTemplate), typeof(Card), new PropertyMetadata(null));
+		#endregion
+
+		#region ExpanderContent and ExpanderContentTemplate
+		public object ExpanderContent
+		{
+			get { return (object)GetValue(ExpanderContentProperty); }
+			set { SetValue(ExpanderContentProperty, value); }
+		}
+
+		public static readonly DependencyProperty ExpanderContentProperty =
+			DependencyProperty.Register("ExpanderContent", typeof(object), typeof(Card), new PropertyMetadata(null));
+
+		public DataTemplate ExpanderContentTemplate
+		{
+			get { return (DataTemplate)GetValue(ExpanderContentTemplateProperty); }
+			set { SetValue(ExpanderContentTemplateProperty, value); }
+		}
+
+		public static readonly DependencyProperty ExpanderContentTemplateProperty =
+			DependencyProperty.Register("ExpanderContentTemplate", typeof(DataTemplate), typeof(Card), new PropertyMetadata(null));
+		#endregion
+
+		#region IsOpened
+		public bool IsOpened
+		{
+			get { return (bool)GetValue(IsOpenedProperty); }
+			set { SetValue(IsOpenedProperty, value); }
+		}
+
+		public static readonly DependencyProperty IsOpenedProperty =
+			DependencyProperty.Register("IsOpened", typeof(bool), typeof(Card), new PropertyMetadata(false, OnIsOpenedChanged));
+
+		private static void OnIsOpenedChanged(object d, DependencyPropertyChangedEventArgs e)
+		{
+			var card = d as Card;
+			card?.UpdateOpenVisualStates(true);
+		}
+		#endregion
+
+		#region CloseWhenUnloaded
+		public bool CloseWhenUnloaded
+		{
+			get { return (bool)GetValue(CloseWhenUnloadedProperty); }
+			set { SetValue(CloseWhenUnloadedProperty, value); }
+		}
+
+		public static readonly DependencyProperty CloseWhenUnloadedProperty =
+			DependencyProperty.Register("CloseWhenUnloaded", typeof(bool), typeof(Card), new PropertyMetadata(false));
+		#endregion
+
+		#region PresenterHeight
+		public double PresenterHeight
+		{
+			get { return (double)GetValue(PresenterHeightProperty); }
+			set { SetValue(PresenterHeightProperty, value); }
+		}
+
+		public static readonly DependencyProperty PresenterHeightProperty =
+			DependencyProperty.Register("PresenterHeight", typeof(double), typeof(Card), new PropertyMetadata(0d, OnPresenterHeightChanged));
+
+		private static void OnPresenterHeightChanged(object d, DependencyPropertyChangedEventArgs e)
+		{
+			var card = d as Card;
+			card?.UpdateOpenVisualStates(true);
+			card?.UpdatePresenterHeight(0);
+		}
+		#endregion
+
+		#region Elevation
+		public double Elevation
+		{
+			get { return (double)GetValue(ElevationProperty); }
+			set { SetValue(ElevationProperty, value); }
+		}
+
+		public static readonly DependencyProperty ElevationProperty =
+			DependencyProperty.Register("Elevation", typeof(double), typeof(Card), new PropertyMetadata(0));
+		#endregion
+	}
+}
--- src/library/Uno.Material/Controls/Card.cs
+++ src/library/Uno.Material/Controls/Card.cs
@@ -1,182 +1,18 @@
-﻿using Windows.Foundation;
+﻿using Windows.ApplicationModel.Store;
+using Windows.Foundation;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
 
 namespace Uno.Material.Controls
 {
-	[TemplateVisualState(GroupName = __openStateGroup, Name = __openedState)]
-	[TemplateVisualState(GroupName = __openStateGroup, Name = __closedState)]
+	[TemplateVisualState(GroupName = OpenStateGroup, Name = OpenedState)]
+	[TemplateVisualState(GroupName = OpenStateGroup, Name = ClosedState)]
 	public partial class Card : Control
 	{
-		private const string __openStateGroup = "OpenStates";
-		private const string __openedState = "Opened";
-		private const string __closedState = "Closed";
-
-		#region Headear and SubHeader
-		public string Header
-		{
-			get { return (string)GetValue(HeaderProperty); }
-			set { SetValue(HeaderProperty, value); }
-		}
-
-		public static readonly DependencyProperty HeaderProperty =
-			DependencyProperty.Register("Header", typeof(string), typeof(Card), new PropertyMetadata(string.Empty));
-
-		public string SubHeader
-		{
-			get { return (string)GetValue(SubHeaderProperty); }
-			set { SetValue(SubHeaderProperty, value); }
-		}
-
-		public static readonly DependencyProperty SubHeaderProperty =
-			DependencyProperty.Register("SubHeader", typeof(string), typeof(Card), new PropertyMetadata(string.Empty));
-		#endregion
-
-		#region AvatarContent and AvatarContentTemplate
-		public object AvatarContent
-		{
-			get { return (object)GetValue(AvatarContentProperty); }
-			set { SetValue(AvatarContentProperty, value); }
-		}
-
-		public static readonly DependencyProperty AvatarContentProperty =
-			DependencyProperty.Register("AvatarContent", typeof(object), typeof(Card), new PropertyMetadata(null));
-
-		public DataTemplate AvatarContentTemplate
-		{
-			get { return (DataTemplate)GetValue(AvatarContentTemplateProperty); }
-			set { SetValue(AvatarContentTemplateProperty, value); }
-		}
-		public static readonly DependencyProperty AvatarContentTemplateProperty =
-			DependencyProperty.Register("AvatarContentTemplate", typeof(DataTemplate), typeof(Card), new PropertyMetadata(null));
-		#endregion
-
-		#region MediaContent and MediaContentTemplate
-		public object MediaContent
-		{
-			get { return (object)GetValue(MediaContentProperty); }
-			set { SetValue(MediaContentProperty, value); }
-		}
-
-		public static readonly DependencyProperty MediaContentProperty =
-			DependencyProperty.Register("MediaContent", typeof(object), typeof(Card), new PropertyMetadata(null));
-
-		public DataTemplate MediaContentTemplate
-		{
-			get { return (DataTemplate)GetValue(MediaContentTemplateProperty); }
-			set { SetValue(MediaContentTemplateProperty, value); }
-		}
-
-		public static readonly DependencyProperty MediaContentTemplateProperty =
-			DependencyProperty.Register("MediaContentTemplate", typeof(DataTemplate), typeof(Card), new PropertyMetadata(null));
-		#endregion
-
-		#region SupportingText
-		public string SupportingText
-		{
-			get { return (string)GetValue(SupportingTextProperty); }
-			set { SetValue(SupportingTextProperty, value); }
-		}
-
-		public static readonly DependencyProperty SupportingTextProperty =
-			DependencyProperty.Register("SupportingText", typeof(string), typeof(Card), new PropertyMetadata(string.Empty));
-		#endregion
-
-		#region ButtonsItemsSource and IconsItemsSource
-		public object ButtonsItemsSource
-		{
-			get { return (object)GetValue(ButtonsItemsSourceProperty); }
-			set { SetValue(ButtonsItemsSourceProperty, value); }
-		}
-
-		public static readonly DependencyProperty ButtonsItemsSourceProperty =
-			DependencyProperty.Register("ButtonsItemsSource", typeof(object), typeof(Card), new PropertyMetadata(null));
-
-		public object IconsItemsSource
-		{
-			get { return (object)GetValue(IconsItemsSourceProperty); }
-			set { SetValue(IconsItemsSourceProperty, value); }
-		}
-
-		public static readonly DependencyProperty IconsItemsSourceProperty =
-			DependencyProperty.Register("IconsItemsSource", typeof(object), typeof(Card), new PropertyMetadata(null));
-		#endregion
-
-		#region ExpanderHeaderContent, ExpanderContent and ExpanderContentTemplate
-		public object ExpanderHeaderContent
-		{
-			get { return (object)GetValue(ExpanderHeaderContentProperty); }
-			set { SetValue(ExpanderHeaderContentProperty, value); }
-		}
-
-		public static readonly DependencyProperty ExpanderHeaderContentProperty =
-			DependencyProperty.Register("ExpanderHeaderContent", typeof(object), typeof(Card), new PropertyMetadata(null));
-
-		public object ExpanderContent
-		{
-			get { return (object)GetValue(ExpanderContentProperty); }
-			set { SetValue(ExpanderContentProperty, value); }
-		}
-
-		public static readonly DependencyProperty ExpanderContentProperty =
-			DependencyProperty.Register("ExpanderContent", typeof(object), typeof(Card), new PropertyMetadata(null));
-
-		public DataTemplate ExpanderContentTemplate
-		{
-			get { return (DataTemplate)GetValue(ExpanderContentTemplateProperty); }
-			set { SetValue(ExpanderContentTemplateProperty, value); }
-		}
-
-		public static readonly DependencyProperty ExpanderContentTemplateProperty =
-			DependencyProperty.Register("ExpanderContentTemplate", typeof(DataTemplate), typeof(Card), new PropertyMetadata(null));
-		#endregion
-
-		#region IsOpened
-		public bool IsOpened
-		{
-			get { return (bool)GetValue(IsOpenedProperty); }
-			set { SetValue(IsOpenedProperty, value); }
-		}
-
-		public static readonly DependencyProperty IsOpenedProperty =
-			DependencyProperty.Register("IsOpened", typeof(bool), typeof(Card), new PropertyMetadata(false, OnIsOpenedChanged));
-
-		private static void OnIsOpenedChanged(object d, DependencyPropertyChangedEventArgs e)
-		{
-			var card = d as Card;
-			card?.UpdateOpenVisualStates(true);
-		}
-		#endregion
-
-		#region CloseWhenUnloaded
-		public bool CloseWhenUnloaded
-		{
-			get { return (bool)GetValue(CloseWhenUnloadedProperty); }
-			set { SetValue(CloseWhenUnloadedProperty, value); }
-		}
-
-		public static readonly DependencyProperty CloseWhenUnloadedProperty =
-			DependencyProperty.Register("CloseWhenUnloaded", typeof(bool), typeof(Card), new PropertyMetadata(false));
-		#endregion
-
-		#region PresenterHeight
-		public double PresenterHeight
-		{
-			get { return (double)GetValue(PresenterHeightProperty); }
-			set { SetValue(PresenterHeightProperty, value); }
-		}
-
-		public static readonly DependencyProperty PresenterHeightProperty =
-			DependencyProperty.Register("PresenterHeight", typeof(double), typeof(Card), new PropertyMetadata(0d, OnPresenterHeightChanged));
-
-		private static void OnPresenterHeightChanged(object d, DependencyPropertyChangedEventArgs e)
-		{
-			var card = d as Card;
-			card?.UpdateOpenVisualStates(true);
-			card?.UpdatePresenterHeight(0);
-		}
-		#endregion
+		private const string OpenStateGroup = "OpenStates";
+		private const string OpenedState = "Opened";
+		private const string ClosedState = "Closed";
 
 		public Card()
 		{
@@ -239,11 +75,11 @@ namespace Uno.Material.Controls
 		{
 			if (IsOpened)
 			{
-				VisualStateManager.GoToState(this, __openedState, useTransitions);
+				VisualStateManager.GoToState(this, OpenedState, useTransitions);
 			}
 			else
 			{
-				VisualStateManager.GoToState(this, __closedState, useTransitions);
+				VisualStateManager.GoToState(this, ClosedState, useTransitions);
 			}
 		}
 
