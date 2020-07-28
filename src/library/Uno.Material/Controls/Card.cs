b89e5d141da75899caf1b4381a3eae7b145294a0
Agnès Zitte <agnes.zitte@nventive.com>
2020-07-28 19:20:05 -0400

    chore(Cards): Remove expander in Card control and adjust styles/converters
    BREAKING CHANGE: Make sure to update your card control styles in order to remove any content related to expanders (visual states, content, content template,...)

--- src/library/Uno.Material/Controls/Card.cs
+++ src/library/Uno.Material/Controls/Card.cs
@@ -6,20 +6,11 @@ using Windows.UI.Xaml.Input;
 
 namespace Uno.Material.Controls
 {
-	[TemplateVisualState(GroupName = OpenStateGroup, Name = OpenedState)]
-	[TemplateVisualState(GroupName = OpenStateGroup, Name = ClosedState)]
 	public partial class Card : Control
 	{
-		private const string OpenStateGroup = "OpenStates";
-		private const string OpenedState = "Opened";
-		private const string ClosedState = "Closed";
-
 		public Card()
 		{
 			DefaultStyleKey = typeof(Card);
-
-			Loaded += (s, e) => { UpdateOpenVisualStates(false); };
-			Unloaded += TryCloseOnUnloaded;
 		}
 
 		protected override void OnApplyTemplate()
@@ -34,61 +25,6 @@ namespace Uno.Material.Controls
 			}
 
 			base.OnApplyTemplate();
-		}
-
-		protected override Size MeasureOverride(Size availableSize)
-		{
-			var size = base.MeasureOverride(availableSize);
-
-			UpdatePresenterHeight((float)availableSize.Width);
-			return size;
-		}
-
-		protected override Size ArrangeOverride(Size finalSize)
-		{
-			return base.ArrangeOverride(finalSize);
-		}
-
-		protected void UpdatePresenterHeight(double availableWidth)
-		{
-			//No need to recalculate the Height of child
-			if (PresenterHeight == 0)
-			{
-				var element = this.ExpanderContent as FrameworkElement;
-
-				if (element == null)
-				{
-					var presenter = new ContentPresenter() { Content = this.ExpanderContent, ContentTemplate = this.ExpanderContentTemplate };
-
-					presenter.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
-					PresenterHeight = presenter.DesiredSize.Height;
-				}
-				else
-				{
-					element.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
-					PresenterHeight = element.DesiredSize.Height;
-				}
-			}
-		}
-
-		private void UpdateOpenVisualStates(bool useTransitions)
-		{
-			if (IsOpened)
-			{
-				VisualStateManager.GoToState(this, OpenedState, useTransitions);
-			}
-			else
-			{
-				VisualStateManager.GoToState(this, ClosedState, useTransitions);
-			}
-		}
-
-		private void TryCloseOnUnloaded(object sender, RoutedEventArgs e)
-		{
-			if (CloseWhenUnloaded)
-			{
-				IsOpened = false;
-			}
 		}
 
 		protected override void OnPointerEntered(PointerRoutedEventArgs e)
---
+++ src/library/Uno.Material/Converters/FromEmptyStringOrNullObjectToValueConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Uno.Material.Converters
+{
+	public class FromEmptyStringOrNullObjectToValueConverter : IValueConverter
+	{
+		public object EmptyOrNullValue { get; set; }
+
+		public object NotEmptyOrNullValue { get; set; }
+
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			if (!(value is string str) || string.IsNullOrEmpty(str))
+			{
+				return EmptyOrNullValue;
+			}
+
+			if (value == null || value == DependencyProperty.UnsetValue)
+			{
+				return EmptyOrNullValue;
+			}
+
+			return NotEmptyOrNullValue;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			throw new NotSupportedException();
+		}
+	}
+}
