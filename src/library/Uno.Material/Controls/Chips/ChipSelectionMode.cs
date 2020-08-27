b7c8fb28a200a6dbde88f6fccacfee08579eb19d
xiaoy312 <xiaoyao312@gmail.com>
2020-08-27 16:28:56 -0400

    chore(chips): added support for selection

---
+++ src/library/Uno.Material/Controls/Chips/ChipSelectionMode.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uno.Material.Controls
+{
+	public enum ChipSelectionMode
+	{
+		None, Single, Multiple,
+	}
+}
--- src/samples/Uno.Material.Samples/Uno.Material.Samples.Shared/Content/Controls/ChipSamplePage.xaml.cs
+++ src/samples/Uno.Material.Samples/Uno.Material.Samples.Shared/Content/Controls/ChipSamplePage.xaml.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Uno.Material.Controls;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -28,8 +29,38 @@ namespace Uno.Material.Samples.Content.Controls
 		{
 			this.InitializeComponent();
 			this.DataContext = Enumerable.Range(0, 10)
-				.Select(x => new SelectableData { Text = $"Item {x}", IsSelected = x % 2 == 0 })
+				.Select(x => new SelectableData
+				{
+					Text = $"Item {x}",
+					IsSelected = BindingChipBag.SelectionMode == ChipSelectionMode.Multiple && x % 2 == 0
+				})
 				.ToArray();
+		}
+
+		private void ChangeItemsSource(object sender, RoutedEventArgs e)
+		{
+			this.DataContext = Enumerable.Range(0, new Random().Next(8, 12 + 2))
+				.Select(x => new SelectableData
+				{
+					Text = $"Item {x}",
+					IsSelected = BindingChipBag.SelectionMode == ChipSelectionMode.Multiple && x % 2 == 0
+				})
+				.ToArray();
+		}
+
+		private void SetBindingChipBagSelectionMode(object sender, RoutedEventArgs e)
+		{
+			if (sender is RadioButton radio)
+			{
+				switch (radio.Content)
+				{
+					case "None": BindingChipBag.SelectionMode = ChipSelectionMode.None; break;
+					case "Single": BindingChipBag.SelectionMode = ChipSelectionMode.Single; break;
+					case "Multiple": BindingChipBag.SelectionMode = ChipSelectionMode.Multiple; break;
+
+					default: throw new ArgumentOutOfRangeException($"Invalid option: {radio.Content}");
+				}
+			}
 		}
 
 		public class SelectableData : InpcObject
@@ -37,6 +68,8 @@ namespace Uno.Material.Samples.Content.Controls
 			public string Text { get => GetProperty<string>(); set => SetProperty(value); }
 
 			public bool IsSelected { get => GetProperty<bool>(); set => SetProperty(value); }
+
+			public override string ToString() => $"[{(IsSelected ? 'X' : ' ')}] {Text}";
 		}
 
 		public class InpcObject : INotifyPropertyChanged
