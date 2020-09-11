0144979df61ce4ddbb348b5b04df5e5666738f48
Nicolas Champagne <nicolas.champagne@nventive.com>
2020-09-11 11:36:33 -0400

    fix(expandingBottomSheet): Implementation changes, improvements

--- src/library/Uno.Material/Controls/ExpandingBottomSheet.cs
+++ src/library/Uno.Material/Controls/ExpandingBottomSheet.cs
@@ -1,4 +1,5 @@
-﻿using Windows.ApplicationModel.Store;
+﻿using Uno.UI.Toolkit;
+using Windows.ApplicationModel.Store;
 using Windows.Foundation;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -8,15 +9,60 @@ namespace Uno.Material.Controls
 {
 	[TemplateVisualState(GroupName = __sheetStateGroup, Name = __collapsedState)]
 	[TemplateVisualState(GroupName = __sheetStateGroup, Name = __expandedState)]
+	[TemplateVisualState(GroupName = __collapsedStateGroup, Name = __collapsedState)]
+	[TemplateVisualState(GroupName = __collapsedStateGroup, Name = __collapsedContentState)]
+	[TemplateVisualState(GroupName = __collapsedStateGroup, Name = __minimalContentState)]
+	[TemplatePart(Name = CollapsedContentButtonName, Type = typeof(Button))]
+	[TemplatePart(Name = ExpandedContentButtonName, Type = typeof(Button))]
+	[TemplatePart(Name = MinimalCollapsedContentButtonName, Type = typeof(Button))]
+	[TemplatePart(Name = CollapsedContentPresenterName, Type = typeof(ElevatedView))]
+	[TemplatePart(Name = MinimalCollapsedContentPresenterName, Type = typeof(ElevatedView))]
 	public partial class ExpandingBottomSheet : Control
 	{
+		private const string CollapsedContentButtonName = "Part_CollapsedContentButton";
+		private const string ExpandedContentButtonName = "Part_ExpandedContentButton";
+		private const string MinimalCollapsedContentButtonName = "Part_MinimalCollapsedContentButton";
+		private const string CollapsedContentPresenterName = "Part_CollapsedContentPresenter";
+		private const string MinimalCollapsedContentPresenterName = "Part_MinimalCollapsedContentPresenter";
+
 		private const string __sheetStateGroup = "SheetStates";
 		private const string __collapsedState = "Collapsed";
 		private const string __expandedState = "Expanded";
 
+		private const string __collapsedStateGroup = "CollapsedStates";
+		private const string __collapsedContentState = "CollapsedContent";
+		private const string __minimalContentState = "MinimalContent";
+
+		private Button _collapsedContentButton;
+		private Button _expandedContentButton;
+		private Button _minimalCollapsedContentButton;
+		private ElevatedView _collapsedContentPresenter;
+		private ElevatedView _minimalCollapsedContentPresenter;
+
+		private bool _loaded = false;
+		private bool _templateApplied = false;
+
 		public ExpandingBottomSheet()
 		{
 			DefaultStyleKey = typeof(ExpandingBottomSheet);
+			Loaded += OnLoaded;
+		}
+
+		private void OnLoaded(object sender, RoutedEventArgs e)
+		{
+			_loaded = true;
+
+			if (_loaded && _templateApplied)
+			{
+				SetSizeProperties();
+			}
+		}
+
+		private void SetSizeProperties()
+		{
+			PresenterHeight = ActualHeight;
+			CollapsedPresenterWidth = _collapsedContentPresenter.ActualWidth;
+			MinimalCollapsedPresenterWidth = _minimalCollapsedContentPresenter.ActualWidth;
 		}
 
 		protected override void OnApplyTemplate()
@@ -30,10 +76,38 @@ namespace Uno.Material.Controls
 				VisualStateManager.GoToState(this, "Disabled", true);
 			}
 
+			_collapsedContentButton = (Button)GetTemplateChild(CollapsedContentButtonName);
+			_expandedContentButton = (Button)GetTemplateChild(ExpandedContentButtonName);
+			_minimalCollapsedContentButton = (Button)GetTemplateChild(MinimalCollapsedContentButtonName);
+			_collapsedContentPresenter = (ElevatedView)GetTemplateChild(CollapsedContentPresenterName);
+			_minimalCollapsedContentPresenter = (ElevatedView)GetTemplateChild(MinimalCollapsedContentPresenterName);
+
+			if (_collapsedContentButton != null)
+			{
+				_collapsedContentButton.Click += ToggleSheet;
+			}
+
+			if (_expandedContentButton != null)
+			{
+				_expandedContentButton.Click += ToggleSheet;
+			}
+
+			if (_minimalCollapsedContentButton != null)
+			{
+				_minimalCollapsedContentButton.Click += ToggleSheet;
+			}		
+
 			base.OnApplyTemplate();
+
+			_templateApplied = true;
+
+			if (_loaded && _templateApplied)
+			{
+				SetSizeProperties();
+			}
 		}
 
-		private void UpdateVisualStates(bool useTransitions)
+		private void UpdateSheetVisualStates(bool useTransitions)
 		{
 			if (IsExpanded)
 			{
@@ -42,6 +116,18 @@ namespace Uno.Material.Controls
 			else
 			{
 				VisualStateManager.GoToState(this, __collapsedState, useTransitions);
+			}
+		}
+
+		private void UpdateCollapsedVisualStates(bool useTransitions)
+		{
+			if (IsMinimalSheet)
+			{
+				VisualStateManager.GoToState(this, __minimalContentState, useTransitions);
+			}
+			else
+			{
+				VisualStateManager.GoToState(this, __collapsedContentState, useTransitions);
 			}
 		}
 
@@ -63,7 +149,7 @@ namespace Uno.Material.Controls
 		private static void OnIsExpandedChanged(object d, DependencyPropertyChangedEventArgs e)
 		{
 			var sheet = d as ExpandingBottomSheet;
-			sheet?.UpdateVisualStates(true);
+			sheet?.UpdateSheetVisualStates(true);
 		}
 		#endregion
 
@@ -125,6 +211,55 @@ namespace Uno.Material.Controls
 			DependencyProperty.Register("ExpandedHeaderContentTemplate", typeof(DataTemplate), typeof(ExpandingBottomSheet), new PropertyMetadata(null));
 		#endregion
 
+		#region PresenterHeight, CollapsedPresenterWidth  and MinimalCollapsedPresenterWidth
+		public double PresenterHeight
+		{
+			get { return (double)GetValue(PresenterHeightProperty); }
+			set { SetValue(PresenterHeightProperty, value); }
+		}
+
+		public static readonly DependencyProperty PresenterHeightProperty =
+			DependencyProperty.Register("PresenterHeight", typeof(double), typeof(ExpandingBottomSheet), new PropertyMetadata(0d, null));
+
+		public double CollapsedPresenterWidth
+		{
+			get { return (double)GetValue(CollapsedPresenterWidthProperty); }
+			set { SetValue(CollapsedPresenterWidthProperty, value); }
+		}
+
+		public static readonly DependencyProperty CollapsedPresenterWidthProperty =
+			DependencyProperty.Register("CollapsedPresenterWidth", typeof(double), typeof(ExpandingBottomSheet), new PropertyMetadata(0d, null));
+
+		public double MinimalCollapsedPresenterWidth
+		{
+			get { return (double)GetValue(MinimalCollapsedPresenterWidthProperty); }
+			set { SetValue(MinimalCollapsedPresenterWidthProperty, value); }
+		}
+
+		public static readonly DependencyProperty MinimalCollapsedPresenterWidthProperty =
+			DependencyProperty.Register("MinimalCollapsedPresenterWidth", typeof(double), typeof(ExpandingBottomSheet), new PropertyMetadata(0d, null));
+		#endregion
+
+		#region FullScreenPresenterHeight, CollapsedPresenterWidth  and MinimalCollapsedPresenterWidth
+		public double FullScreenPresenterHeight
+		{
+			get { return (double)GetValue(FullScreenPresenterHeightProperty); }
+			set { SetValue(FullScreenPresenterHeightProperty, value); }
+		}
+
+		public static readonly DependencyProperty FullScreenPresenterHeightProperty =
+			DependencyProperty.Register("FullScreenPresenterHeight", typeof(double), typeof(ExpandingBottomSheet), new PropertyMetadata(400d, null));
+
+		public double FullScreenPresenterWidth
+		{
+			get { return (double)GetValue(FullScreenPresenterWidthProperty); }
+			set { SetValue(FullScreenPresenterWidthProperty, value); }
+		}
+
+		public static readonly DependencyProperty FullScreenPresenterWidthProperty =
+			DependencyProperty.Register("FullScreenPresenterWidth", typeof(double), typeof(ExpandingBottomSheet), new PropertyMetadata(400d, null));
+		#endregion
+
 		#region IsMinimalSheet
 		public bool IsMinimalSheet
 		{
@@ -133,7 +268,13 @@ namespace Uno.Material.Controls
 		}
 
 		public static readonly DependencyProperty IsMinimalSheetProperty =
-			DependencyProperty.Register("IsMinimalSheet", typeof(bool), typeof(ExpandingBottomSheet), new PropertyMetadata(false));
+			DependencyProperty.Register("IsMinimalSheet", typeof(bool), typeof(ExpandingBottomSheet), new PropertyMetadata(false, OnIsMinimalChanged));
+
+		private static void OnIsMinimalChanged(object d, DependencyPropertyChangedEventArgs e)
+		{
+			var sheet = d as ExpandingBottomSheet;
+			sheet?.UpdateCollapsedVisualStates(true);
+		}
 		#endregion
 	}
 }
