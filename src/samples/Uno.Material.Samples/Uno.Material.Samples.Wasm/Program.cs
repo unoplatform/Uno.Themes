bbec94a89269e16d16b34ef20bccdc60e325361f
Carl de Billy <carl.debilly@gmail.com>
2021-02-04 09:12:12 -0500

    fix(textbox_icon): The attached property were linked-out on Wasm

--- src/samples/Uno.Material.Samples/Uno.Material.Samples.Wasm/Program.cs
+++ src/samples/Uno.Material.Samples/Uno.Material.Samples.Wasm/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Windows.UI.Xaml;
+using Uno.UI;
 
 namespace Uno.Material.Samples.Wasm
 {
@@ -9,6 +10,9 @@ namespace Uno.Material.Samples.Wasm
 
 		static int Main(string[] args)
 		{
+			FeatureConfiguration.UIElement.AssignDOMXamlName = true;
+			FeatureConfiguration.UIElement.AssignDOMXamlProperties = true;
+
 			Windows.UI.Xaml.Application.Start(_ => _app = new App());
 
 			return 0;
