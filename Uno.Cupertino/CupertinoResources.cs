1406d54cf4cba9e835d1d7d69aa4df2c5f3d4301
Steve Bilogan <steve.bilogan@gmail.com>
2021-02-10 09:32:52 -0500

    feat: Add Cupertino library

---
+++ Uno.Cupertino/CupertinoResources.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace Uno.Cupertino
+{
+	/// <summary>
+	/// Cupertino resources including colors, layout values and styles
+	/// </summary>
+	public sealed class CupertinoResources : ResourceDictionary
+	{
+		public CupertinoResources()
+		{
+			// Add all ResourceDictionaries for Variables here in alphabetical order
+			//this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///Uno.Material/Styles/Application/AnimationConstants.xaml") });
+			//this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///Uno.Material/Styles/Application/Colors.xaml") });
+			//this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///Uno.Material/Styles/Application/Converters.xaml") });
+			//this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///Uno.Material/Styles/Application/TextBoxVariables.xaml") });
+
+			// Add all ResourceDictionaries for Controls here in alphabetical order
+			//this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///Uno.Material/Styles/Controls/Button.xaml") });
+
+		}
+	}
+}
