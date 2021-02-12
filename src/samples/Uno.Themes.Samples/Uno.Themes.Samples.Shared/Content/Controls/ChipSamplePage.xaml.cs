739d90e5b2a071c72b9a73abd8a5e8dcd35ed3e8
Steve Bilogan <steve.bilogan@gmail.com>
2021-02-12 09:14:01 -0500

    Fix x:bind

--- src/samples/Uno.Themes.Samples/Uno.Themes.Samples.Shared/Content/Controls/ChipSamplePage.xaml.cs
+++ src/samples/Uno.Themes.Samples/Uno.Themes.Samples.Shared/Content/Controls/ChipSamplePage.xaml.cs
@@ -17,71 +17,40 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Uno.Themes.Samples.Entities;
+using Uno.Themes.Samples.Entities.Data;
 
 namespace Uno.Themes.Samples.Content.Controls
 {
-	[SamplePage(SampleCategory.Controls, "Chip")]
+	[SamplePage(SampleCategory.Controls, "Chip", SourceSdk.UnoMaterial, DataType = typeof(TestCollections))]
 	public sealed partial class ChipSamplePage : Page
 	{
-		public ObservableCollection<SelectableData> MutableTestCollection { get; } = new ObservableCollection<SelectableData>(CreateItems());
-		public ObservableCollection<SelectableData> TestCollection { get; } = new ObservableCollection<SelectableData>(CreateItems());
-
 		public ChipSamplePage()
 		{
 			this.InitializeComponent();
 		}
 
-		private static IEnumerable<SelectableData> CreateItems()
-		{
-			return Enumerable.Range(0, 10)
-				.Select(x => new SelectableData
-				{
-					Index = x,
-					Image = new Uri("ms-appx:///Assets/Cards/Avatar.png"),
-				})
-				.ToArray();
-		}
-
 		private void RemoveChipItem(object sender, ChipItemEventArgs e)
 		{
-			MutableTestCollection.Remove(e.Item as SelectableData);
+			if (DataContext is Sample sample)
+			{
+				if (sample.Data is TestCollections test)
+				{
+					test.RemoveChipItem(e.Item as TestCollections.SelectableData);
+				}
+			}
 		}
 
 		private void ResetChipItems(object sender, RoutedEventArgs e)
 		{
-			MutableTestCollection.Clear();
-			foreach (var item in CreateItems())
+			if (DataContext is Sample sample)
 			{
-				MutableTestCollection.Add(item);
-			}
-		}
-
-		public class SelectableData : InpcObject
-		{
-			public int Index { get => GetProperty<int>(); set => SetProperty(value); }
-
-			public Uri Image { get => GetProperty<Uri>(); set => SetProperty(value); }
-		}
-
-		public class InpcObject : INotifyPropertyChanged
-		{
-			public event PropertyChangedEventHandler PropertyChanged;
-
-			private readonly Dictionary<string, object> _propertyValueStore = new Dictionary<string, object>();
-
-			protected T GetProperty<T>([CallerMemberName] string propertyName = null)
-			{
-				return _propertyValueStore.TryGetValue(propertyName, out var value) ? (T)value : default;
-			}
-
-			protected void SetProperty<T>(T value, [CallerMemberName] string propertyName = null)
-			{
-				if (!(_propertyValueStore.TryGetValue(propertyName, out var oldValue) && oldValue?.Equals(value) == true))
+				if (sample.Data is TestCollections test)
 				{
-					_propertyValueStore[propertyName] = value;
-					PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+					test.ResetChipItems();
 				}
 			}
 		}
+
+		
 	}
 }
