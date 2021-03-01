00a5125fc0e10a11a694c72089f2722e78d26bf9
Steve Bilogan <steve.bilogan@gmail.com>
2021-03-01 16:01:45 -0500

    wip

--- src/samples/Uno.Themes.Samples/Uno.Themes.Samples.Shared/Content/Controls/DatePickerSamplePage.xaml.cs
+++ src/samples/Uno.Themes.Samples/Uno.Themes.Samples.Shared/Content/Controls/DatePickerSamplePage.xaml.cs
@@ -16,9 +16,7 @@ using Uno.Themes.Samples.Entities;
 
 namespace Uno.Themes.Samples.Content.Controls
 {
-#if !__WASM__ && !__MACOS__
 	[SamplePage(SampleCategory.Controls, "DatePicker", Description = "This control allows users to pick a date value.", DocumentationLink = "https://docs.microsoft.com/en-us/uwp/api/windows.ui.xaml.controls.datepicker")]
-#endif
 	public sealed partial class DatePickerSamplePage : Page
 	{
 		public DatePickerSamplePage()
