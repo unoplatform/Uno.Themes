28353919ba09648e2f2f0c6f026ac8987bd2f5dd
Marc-Antoine Therrien <ma.therrien@nventive.com>
2021-03-02 10:34:44 -0500

    style: Add TextBox style

---
+++ src/library/Uno.Cupertino/Converters/FromEmptyStringToValueConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Data;
+
+namespace Uno.Cupertino.Converters
+{
+	public class FromEmptyStringToValueConverter : IValueConverter
+	{
+		public object NullOrEmptyValue { get; set; }
+
+		public object NotNullOrEmptyValue { get; set; }
+
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			if (!(value is string str) || string.IsNullOrEmpty(str))
+			{
+				return NullOrEmptyValue;
+			}
+
+			return NotNullOrEmptyValue;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			throw new NotSupportedException();
+		}
+	}
+}
