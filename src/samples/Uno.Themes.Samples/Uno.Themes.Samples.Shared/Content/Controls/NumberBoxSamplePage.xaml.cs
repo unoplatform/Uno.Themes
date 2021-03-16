cddff11f7552071a78f6e89c37853bf7d7781a9e
Alexandra-Maude Grenier <am.grenier@nventive.com>
2021-03-16 17:33:09 -0400

    feat: Numberbox

---
+++ src/samples/Uno.Themes.Samples/Uno.Themes.Samples.Shared/Content/Controls/NumberBoxSamplePage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Uno.Themes.Samples.Entities;
+using Windows.UI.Xaml.Controls;
+
+namespace Uno.Themes.Samples.Content.Controls
+{
+[SamplePage(SampleCategory.Controls, "Number Box", Description = "This control can be used to set a numeric value.", DocumentationLink = "https://docs.microsoft.com/en-us/windows/winui/api/microsoft.ui.xaml.controls.infobar?view=winui-2.5")]
+	public sealed partial class NumberBoxSamplePage : Page
+    {
+        public NumberBoxSamplePage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
