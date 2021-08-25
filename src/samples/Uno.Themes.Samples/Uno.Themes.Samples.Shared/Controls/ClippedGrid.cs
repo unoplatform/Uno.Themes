3ce2bef1d5eb862b916b5dde95f7ae33728bd084
xiaoy312 <xiaoyao312@gmail.com>
2021-08-25 11:58:22 -0400

    fix: sample control clipping

---
+++ src/samples/Uno.Themes.Samples/Uno.Themes.Samples.Shared/Controls/ClippedGrid.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.Foundation;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace Uno.Themes.Samples
+{
+	public partial class ClippedGrid : Grid
+	{
+		public ClippedGrid()
+		{
+			Loaded += (s, e) => UpdateClippingArea();
+			SizeChanged += (s, e) => UpdateClippingArea();
+		}
+
+		private void UpdateClippingArea()
+		{
+			var rect = new Rect(0, 0, ActualHeight, ActualWidth);
+
+			if (Clip != null)
+			{
+				Clip.Rect = rect;
+			}
+			else
+			{
+				Clip = new RectangleGeometry { Rect = rect };
+			}
+		}
+	}
+}
