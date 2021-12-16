4650de47f921df3704c5af5042af563b596705cd
Jerome Laban <jerome.laban@nventive.com>
2021-12-16 16:49:46 -0500

    perf: Improve resources lookup using merged `ResourceDictionary` for styles

--- src/library/Uno.Cupertino/CupertinoResources.cs
+++ src/library/Uno.Cupertino/CupertinoResources.cs
@@ -27,35 +27,50 @@ namespace Uno.Cupertino
 		private IEnumerable<(string Source, string[] ImplicitStyles)> GetResourceInfos()
 		{
 			var resources = new List<(string Path, string[] ImplicitStyles)>();
-			// Add all ResourceDictionaries for Variables here in alphabetical order
-			Add("ms-appx:///Uno.Cupertino/Styles/Application/AnimationConstants.xaml");
-			Add("ms-appx:///Uno.Cupertino/Styles/Application/Colors.xaml");
-			Add("ms-appx:///Uno.Cupertino/Styles/Application/Converters.xaml");
-			Add("ms-appx:///Uno.Cupertino/Styles/Application/StateConstants.xaml");
 
-			// Add all ResourceDictionaries for Controls here in alphabetical order
-			Add("ms-appx:///Uno.Cupertino/Styles/Controls/Button.xaml", "CupertinoButtonStyle");
-			Add("ms-appx:///Uno.Cupertino/Styles/Controls/CalendarDatePicker.xaml", "CupertinoCalendarDatePickerStyle");
-			Add("ms-appx:///Uno.Cupertino/Styles/Controls/CalendarView.xaml", "CupertinoCalendarViewStyle");
-			Add("ms-appx:///Uno.Cupertino/Styles/Controls/CheckBox.xaml", "CupertinoCheckBoxStyle");
-			Add("ms-appx:///Uno.Cupertino/Styles/Controls/ComboBox.xaml", "CupertinoComboBoxStyle");
-			Add("ms-appx:///Uno.Cupertino/Styles/Controls/DatePicker.xaml", "CupertinoDatePickerStyle");
-			Add("ms-appx:///Uno.Cupertino/Styles/Controls/HyperlinkButton.xaml", "CupertinoHyperlinkButtonStyle");
-			Add("ms-appx:///Uno.Cupertino/Styles/Controls/NumberBox.xaml", "CupertinoNumberBoxStyle");
-			Add("ms-appx:///Uno.Cupertino/Styles/Controls/PasswordBox.xaml", "CupertinoPasswordBoxStyle");
-			Add("ms-appx:///Uno.Cupertino/Styles/Controls/ProgressBar.xaml", "CupertinoProgressBarStyle");
+			var implicitStyles = new[] {
+				// Add all ResourceDictionaries for Controls here in alphabetical order
+				// "ms-appx:///Uno.Cupertino/Styles/Controls/Button.xaml",
+				"CupertinoButtonStyle",
+				// "ms-appx:///Uno.Cupertino/Styles/Controls/CalendarDatePicker.xaml",
+				"CupertinoCalendarDatePickerStyle",
+				// "ms-appx:///Uno.Cupertino/Styles/Controls/CalendarView.xaml",
+				"CupertinoCalendarViewStyle",
+				// "ms-appx:///Uno.Cupertino/Styles/Controls/CheckBox.xaml",
+				"CupertinoCheckBoxStyle",
+				// "ms-appx:///Uno.Cupertino/Styles/Controls/ComboBox.xaml",
+				"CupertinoComboBoxStyle",
+				// "ms-appx:///Uno.Cupertino/Styles/Controls/DatePicker.xaml",
+				"CupertinoDatePickerStyle",
+				// "ms-appx:///Uno.Cupertino/Styles/Controls/HyperlinkButton.xaml",
+				"CupertinoHyperlinkButtonStyle",
+				// "ms-appx:///Uno.Cupertino/Styles/Controls/NumberBox.xaml",
+				"CupertinoNumberBoxStyle",
+				// "ms-appx:///Uno.Cupertino/Styles/Controls/PasswordBox.xaml",
+				"CupertinoPasswordBoxStyle",
+				// "ms-appx:///Uno.Cupertino/Styles/Controls/ProgressBar.xaml",
+				"CupertinoProgressBarStyle",
 #if !WinUI_Desktop
-			Add("ms-appx:///Uno.Cupertino/Styles/Controls/ProgressRing.xaml", "CupertinoProgressRingStyle");
+				// "ms-appx:///Uno.Cupertino/Styles/Controls/ProgressRing.xaml",
+				"CupertinoProgressRingStyle",
 #endif
-			Add("ms-appx:///Uno.Cupertino/Styles/Controls/RadioButton.xaml", "CupertinoRadioButtonStyle");
-			Add("ms-appx:///Uno.Cupertino/Styles/Controls/Slider.xaml", "CupertinoSliderStyle");
-			Add("ms-appx:///Uno.Cupertino/Styles/Controls/TextBlock.xaml", "CupertinoBody");
-			Add("ms-appx:///Uno.Cupertino/Styles/Controls/TextBox.xaml", "CupertinoTextBoxStyle");
-			Add("ms-appx:///Uno.Cupertino/Styles/Controls/ToggleSwitch.xaml", "CupertinoToggleSwitchStyle");
+				// "ms-appx:///Uno.Cupertino/Styles/Controls/RadioButton.xaml",
+				"CupertinoRadioButtonStyle",
+				// "ms-appx:///Uno.Cupertino/Styles/Controls/Slider.xaml",
+				"CupertinoSliderStyle",
+				// "ms-appx:///Uno.Cupertino/Styles/Controls/TextBlock.xaml",
+				"CupertinoBody",
+				// "ms-appx:///Uno.Cupertino/Styles/Controls/TextBox.xaml",
+				"CupertinoTextBoxStyle",
+				// "ms-appx:///Uno.Cupertino/Styles/Controls/ToggleSwitch.xaml",
+				"CupertinoToggleSwitchStyle",
+			};
+
+			Add("ms-appx:///Uno.Cupertino/Generated/mergedpages.xaml", implicitStyles);
 
 			return resources;
 
-			void Add(string source, params string[] implicitStyles) => resources.Add((source, implicitStyles));
+			void Add(string source, params string[] styles) => resources.Add((source, styles));
 		}
 
 		private void ImportResourceDictionaries()
--- src/library/Uno.Material/MaterialResources.cs
+++ src/library/Uno.Material/MaterialResources.cs
@@ -17,96 +17,84 @@ namespace Uno.Material
 	{
 		public MaterialResources()
 		{
-			ImportResourceDictionaries();
+			Source = new Uri("ms-appx:///Uno.Material/Generated/mergedpages.xaml");
 		}
 
 		public bool WithImplicitStyles { set => ExportImplicitStyles(value); }
 
-		private IEnumerable<(string Source, string[] ImplicitStyles)> GetResourceInfos()
+		private string[] GetImplicitStyles()
 		{
-			var resources = new List<(string Path, string[] ImplicitStyles)>();
-
-			// Add all ResourceDictionaries for Variables here in alphabetical order
-			Add("ms-appx:///Uno.Material/Styles/Application/AnimationConstants.xaml");
-			Add("ms-appx:///Uno.Material/Styles/Application/Converters.xaml");
-			Add("ms-appx:///Uno.Material/Styles/Application/TextBoxVariables.xaml");
-
-			const string MobileSuffix =
-#if __ANDROID__ || __IOS__
-				".Mobile";
-#else
-				"";
-#endif
-
-			// Add all ResourceDictionaries for Controls here in alphabetical order
-			Add("ms-appx:///Uno.Material/Styles/Controls/Button.xaml", "MaterialContainedButtonStyle");
-			Add("ms-appx:///Uno.Material/Styles/Controls/CalendarView.xaml", "MaterialCalendarViewStyle");
-			Add("ms-appx:///Uno.Material/Styles/Controls/CalendarDatePicker.xaml", "MaterialCalendarDatePickerStyle");
-			Add("ms-appx:///Uno.Material/Styles/Controls/CheckBox.xaml", "MaterialCheckBoxStyle");
-			Add("ms-appx:///Uno.Material/Styles/Controls/ComboBox.xaml", "MaterialComboBoxStyle");
-			Add("ms-appx:///Uno.Material/Styles/Controls/CommandBar.xaml", "MaterialCommandBarStyle", "MaterialAppBarButton");
-			Add("ms-appx:///Uno.Material/Styles/Controls/DatePicker.xaml", "MaterialDatePickerStyle");
-			Add("ms-appx:///Uno.Material/Styles/Controls/FloatingActionButton.xaml"); // no implicit style, as the target-type is Button
-			Add("ms-appx:///Uno.Material/Styles/Controls/Flyout.xaml", "MaterialFlyoutPresenterStyle", "MaterialMenuFlyoutPresenterStyle");
-			Add("ms-appx:///Uno.Material/Styles/Controls/HyperlinkButton.xaml", "MaterialHyperlinkButtonStyle");
-			Add("ms-appx:///Uno.Material/Styles/Controls/InfoBar.xaml", "MaterialInfoBarStyle");
-			Add("ms-appx:///Uno.Material/Styles/Controls/ListView.xaml"); // TODO: add implicit styles once tested: MaterialListViewStyle, MaterialListViewItemStyle
-#if !WinUI
-			Add("ms-appx:///Uno.Material/Styles/Controls/NavigationView/WUX/NavigationView.xaml", "MaterialWUXNavigationViewStyle"); // NavigationView merges it's related dictionaries already
-#endif
-			Add("ms-appx:///Uno.Material/Styles/Controls/NavigationView/NavigationView_MUX.xaml", "MaterialNavigationViewStyle", "MaterialNavigationViewItemStyle");
-			Add("ms-appx:///Uno.Material/Styles/Controls/PasswordBox.xaml", "MaterialFilledPasswordBoxStyle");
-			Add("ms-appx:///Uno.Material/Styles/Controls/ProgressBar.xaml", "MaterialProgressBarStyle");
-#if !WinUI_Desktop
-			Add("ms-appx:///Uno.Material/Styles/Controls/ProgressRing.xaml", "MaterialProgressRingStyle");
-#endif
-			Add("ms-appx:///Uno.Material/Styles/Controls/RadioButton.xaml", "MaterialRadioButtonStyle");
-			Add("ms-appx:///Uno.Material/Styles/Controls/RatingControl.xaml", "MaterialRatingControlStyle");
-			Add("ms-appx:///Uno.Material/Styles/Controls/Ripple.xaml"); // default implicit: (not-keyed)
-			Add("ms-appx:///Uno.Material/Styles/Controls/Slider.xaml", "MaterialSliderStyle");
-			Add("ms-appx:///Uno.Material/Styles/Controls/TextBox.xaml", "MaterialFilledTextBoxStyle");
-			Add("ms-appx:///Uno.Material/Styles/Controls/TextBlock.xaml", "MaterialBody2");
-			Add("ms-appx:///Uno.Material/Styles/Controls/TimePicker.xaml", "MaterialTimePickerStyle");
-			Add("ms-appx:///Uno.Material/Styles/Controls/ToggleButton.xaml", "MaterialTextToggleButtonStyle");
-			Add($"ms-appx:///Uno.Material/Styles/Controls/ToggleSwitch{MobileSuffix}.xaml", "MaterialToggleSwitchStyle");
-
-			return resources;
-
-			void Add(string source, params string[] implicitStyles) => resources.Add((source, implicitStyles));
+			return new[] {
+				// "ms-appx:///Uno.Material/Styles/Controls/Button.xaml",
+				"MaterialContainedButtonStyle",
+				// "ms-appx:///Uno.Material/Styles/Controls/CalendarView.xaml",
+				"MaterialCalendarViewStyle",
+				// "ms-appx:///Uno.Material/Styles/Controls/CalendarDatePicker.xaml",
+				"MaterialCalendarDatePickerStyle",
+				// "ms-appx:///Uno.Material/Styles/Controls/CheckBox.xaml",
+				"MaterialCheckBoxStyle",
+				// "ms-appx:///Uno.Material/Styles/Controls/ComboBox.xaml",
+				"MaterialComboBoxStyle",
+				// "ms-appx:///Uno.Material/Styles/Controls/CommandBar.xaml",
+				"MaterialCommandBarStyle", "MaterialAppBarButton",
+				// "ms-appx:///Uno.Material/Styles/Controls/DatePicker.xaml",
+				"MaterialDatePickerStyle",
+				// "ms-appx:///Uno.Material/Styles/Controls/FloatingActionButton.xaml", // no implicit style, as the target-type is Button
+				// "ms-appx:///Uno.Material/Styles/Controls/Flyout.xaml",
+				"MaterialFlyoutPresenterStyle", "MaterialMenuFlyoutPresenterStyle",
+				// "ms-appx:///Uno.Material/Styles/Controls/HyperlinkButton.xaml",
+				"MaterialHyperlinkButtonStyle",
+				// "ms-appx:///Uno.Material/Styles/Controls/InfoBar.xaml",
+				"MaterialInfoBarStyle",
+				// "ms-appx:///Uno.Material/Styles/Controls/ListView.xaml", // TODO: add implicit styles once tested: MaterialListViewStyle, MaterialListViewItemStyle
+				// "ms-appx:///Uno.Material/Styles/Controls/NavigationView/NavigationView_MUX.xaml",
+				"MaterialNavigationViewStyle", "MaterialNavigationViewItemStyle",
+				// "ms-appx:///Uno.Material/Styles/Controls/PasswordBox.xaml",
+				"MaterialFilledPasswordBoxStyle",
+				// "ms-appx:///Uno.Material/Styles/Controls/ProgressBar.xaml",
+				"MaterialProgressBarStyle",
+				// "ms-appx:///Uno.Material/Styles/Controls/RadioButton.xaml",
+				"MaterialRadioButtonStyle",
+				// "ms-appx:///Uno.Material/Styles/Controls/RatingControl.xaml",
+				"MaterialRatingControlStyle",
+				// "ms-appx:///Uno.Material/Styles/Controls/Ripple.xaml", // default implicit: (not-keyed)
+				// "ms-appx:///Uno.Material/Styles/Controls/Slider.xaml",
+				"MaterialSliderStyle",
+				// "ms-appx:///Uno.Material/Styles/Controls/TextBox.xaml",
+				"MaterialFilledTextBoxStyle",
+				// "ms-appx:///Uno.Material/Styles/Controls/TextBlock.xaml",
+				"MaterialBody2",
+				// "ms-appx:///Uno.Material/Styles/Controls/TimePicker.xaml",
+				"MaterialTimePickerStyle",
+				// "ms-appx:///Uno.Material/Styles/Controls/ToggleButton.xaml",
+				"MaterialTextToggleButtonStyle",
+				// $"ms-appx:///Uno.Material/Styles/Controls/ToggleSwitch{MobileSuffix}.xaml",
+				"MaterialToggleSwitchStyle",
+			};
 		}
 
-		private void ImportResourceDictionaries()
-		{
-			foreach (var info in GetResourceInfos())
-			{
-				MergedDictionaries.Add(new ResourceDictionary { Source = new Uri(info.Source) });
-			}
-		}
 
 		private void ExportImplicitStyles(bool value)
 		{
 			if (!value) return; // we don't support teardown
 
-			foreach (var info in GetResourceInfos())
+			foreach (var key in GetImplicitStyles())
 			{
-				foreach (var key in info.ImplicitStyles ?? Array.Empty<string>())
+				if (!this.TryGetValue(key, out var resource) || !(resource is Style style))
 				{
-					if (!this.TryGetValue(key, out var resource) || !(resource is Style style))
-					{
-						// uwp: If the {key} style is clearly defined in {info.Source}, but we can't find it here.
-						// And, that it only happens on uwp, and not other uno platforms.
-						// It means that the style references resources that are not directly included.
-						// This can usually be fixed by including `<MaterialColors xmlns="using:Uno.Material" />` in the MergedDictionaries of {info.Source}.
-						// note: Resources used on Style.Setters need to be directly defined/included, those used in Style.Template dont have to be.
-						throw new ArgumentException($"Missing resource: key={key} from={info.Source}");
-					}
-					if (style.TargetType == null)
-					{
-						throw new InvalidOperationException($"Missing TargetType on style: key={key}");
-					}
-
-					this.Add(style.TargetType, style);
+					// uwp: If the {key} style is clearly defined in {info.Source}, but we can't find it here.
+					// And, that it only happens on uwp, and not other uno platforms.
+					// It means that the style references resources that are not directly included.
+					// This can usually be fixed by including `<MaterialColors xmlns="using:Uno.Material" />` in the MergedDictionaries of {info.Source}.
+					// note: Resources used on Style.Setters need to be directly defined/included, those used in Style.Template dont have to be.
+					throw new ArgumentException($"Missing resource: key={key}");
 				}
+				if (style.TargetType == null)
+				{
+					throw new InvalidOperationException($"Missing TargetType on style: key={key}");
+				}
+
+				this.Add(style.TargetType, style);
 			}
 		}
 	}
