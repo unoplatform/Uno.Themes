40e9613cbb8b08a0d3e1dcf3027320ecbc151c19
xiaoy312 <xiaoyao312@gmail.com>
2022-10-24 18:56:15 -0400

    fix: corrected m3 ContentDialog style

--- src/samples/Uno.Themes.Samples/Uno.Themes.Samples.Shared/Content/Controls/ContentDialogSamplePage.xaml.cs
+++ src/samples/Uno.Themes.Samples/Uno.Themes.Samples.Shared/Content/Controls/ContentDialogSamplePage.xaml.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Uno.Extensions;
 using Uno.Themes.Samples.Entities;
+using Uno.Themes.Samples.Helpers;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Markup;
+using Windows.UI.Xaml.Media;
 
 namespace Uno.Themes.Samples.Content.Controls
 {
@@ -16,6 +21,8 @@ namespace Uno.Themes.Samples.Content.Controls
 	)]
 	public sealed partial class ContentDialogSamplePage : Page
 	{
+		private const string SomeText = "Supporting text should be consice and provide clear action instructions to the users.";
+
 		public ContentDialogSamplePage()
 		{
 			this.InitializeComponent();
@@ -25,9 +32,8 @@ namespace Uno.Themes.Samples.Content.Controls
 		{
 			var mappings = new Dictionary<string, Func<ContentDialog>>
 			{
-				["Alert"] = BuildAlertContentDialog,
-				["Simple"] = BuildSimpleContentDialog,
-				["Confirmation"] = BuildConfirmationContentDialog,
+				[nameof(BuildBasicContentDialog)] = BuildBasicContentDialog,
+				[nameof(BuildRichContentDialog)] = BuildRichContentDialog,
 			};
 
 			if ((sender as Button)?.Tag is string context && mappings.TryGetValue(context, out var builder))
@@ -38,54 +44,78 @@ namespace Uno.Themes.Samples.Content.Controls
 			}
 			else
 			{
-				throw new InvalidOperationException();
+				throw new KeyNotFoundException($"The given key '{(sender as Button)?.Tag as string}' was not present in the dictionary.");
 			}
 		}
 
-		private ContentDialog BuildAlertContentDialog() => new ContentDialog()
+		private ContentDialog BuildBasicContentDialog() => new ContentDialog()
 		{
-			Title = "Dialog header",
-			Content = "Dialog body text",
+			Title = new StackPanel
+			{
+				Spacing = 18,
+				Children =
+				{
+					CreateHeaderIcon(),
+					new ContentControl { Content = "Title" },
+				}
+			},
+			Content = SomeText,
 
-			PrimaryButtonText = "OK",
-			CloseButtonText = "CANCEL",
+			PrimaryButtonText = "Action",
+			CloseButtonText = "Dismiss",
 		};
 
-		private ContentDialog BuildSimpleContentDialog()
+		private ContentDialog BuildRichContentDialog()
 		{
-			var list = new ListView
-			{
-				ItemsSource = Enumerable.Range(1, 3).Select(x => $"Item {x}"),
-				IsItemClickEnabled = true,
-			};
 			var dialog = new ContentDialog()
 			{
-				Title = "Dialog header",
-				Content = list,
+				Title =  new StackPanel
+				{
+					Spacing = 18,
+					Children =
+					{
+						CreateHeaderIcon(),
+						new ContentControl { Content = "Title" },
+					},
+				},
+				Content = VGridLayout(
+					(GridLength.Auto, new TextBlock { Text = SomeText }),
+					(new GridLength(1, GridUnitType.Star), new ListView
+					{
+						ItemsSource = Enumerable.Range(1, 3).Select(x => $"Contact Name {x}"),
+						ItemContainerStyle = (Style)this.Resources["CustomSelectableItemStyle"],
+						SelectionMode = ListViewSelectionMode.Multiple,
+						Margin = new Thickness(0, 24, 0, 0),
+					})
+				),
+
+				PrimaryButtonText = "Action",
+				CloseButtonText = "Dismiss",
 			};
-			list.ItemClick += (s, e) => dialog.Hide();
 
 			return dialog;
+
+			Grid VGridLayout(params (GridLength Height, FrameworkElement Content)[] rows)
+			{
+				var grid = new Grid();
+				foreach (var row in rows)
+				{
+					grid.RowDefinitions.Add(new RowDefinition { Height = row.Height });
+
+					Grid.SetRow(row.Content, grid.RowDefinitions.Count - 1);
+					grid.Children.Add(row.Content);
+				}
+
+				return grid;
+			}
 		}
 
-		private ContentDialog BuildConfirmationContentDialog()
+		private PathIcon CreateHeaderIcon() => new PathIcon
 		{
-			var list = new ListView
-			{
-				ItemsSource = Enumerable.Range(1, 3).Select(x => $"Item {x}"),
-			};
-			var dialog = new ContentDialog()
-			{
-				Title = "Dialog header",
-				Content = list,
-				
-				PrimaryButtonText = "OK",
-				IsPrimaryButtonEnabled = false,
-				CloseButtonText = "CANCEL",
-			};
-			list.SelectionChanged += (s, e) => dialog.IsPrimaryButtonEnabled = list.SelectedIndex != -1;
-
-			return dialog;
-		}
+			Data = (Geometry)XamlBindingHelper.ConvertValue(typeof(Geometry), "M7 0C3.13 0 0 3.13 0 7C0 12.25 7 20 7 20C7 20 14 12.25 14 7C14 3.13 10.87 0 7 0ZM7 9.5C5.62 9.5 4.5 8.38 4.5 7C4.5 5.62 5.62 4.5 7 4.5C8.38 4.5 9.5 5.62 9.5 7C9.5 8.38 8.38 9.5 7 9.5Z"),
+			Foreground = (Brush)XamlBindingHelper.ConvertValue(typeof(SolidColorBrush), "#006C47"),
+			Width = 24,
+			Height = 24,
+		};
 	}
 }
