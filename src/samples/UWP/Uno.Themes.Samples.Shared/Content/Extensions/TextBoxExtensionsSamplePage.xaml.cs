4a7ea748851cb32f2f05712b7ab7d9a3d50826c3
eriklimakc <erik.lima@knowcode.app>
2023-06-02 12:37:03 +0100

    feat: Adding TextBoxExtensions page

---
+++ src/samples/UWP/Uno.Themes.Samples.Shared/Content/Extensions/TextBoxExtensionsSamplePage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Uno.Themes.Samples.Entities;
+using Windows.UI.Xaml.Controls;
+
+namespace Uno.Themes.Samples.Shared.Content.Extensions
+{
+	[SamplePage(SampleCategory.Helpers, "TextBoxExtensions", IconPath = Icons.Helpers.TextBoxExtensions)]
+	public sealed partial class TextBoxExtensionsSamplePage : Page
+	{
+		public TextBoxExtensionsSamplePage()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
