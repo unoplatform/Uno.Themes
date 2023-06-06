40bd161764b1bc45e97ceb83b0fee7cd0ee3b3d5
agneszitte <agnes@platform.uno>
2023-06-05 20:41:30 -0400

    fix: MaterialFilledTextBoxStyle to follow the specs on Windows (other platforms need to be properly tested and namings adjusted)

---
+++ src/library/Uno.Material/Converters/FromTextBoxEmptyStringToValueConverter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Controls;
+
+#if WinUI
+using Microsoft.UI.Xaml.Data;
+#else
+using Windows.UI.Xaml.Data;
+#endif
+
+namespace Uno.Material
+{
+	public class FromTextBoxEmptyStringToValueConverter : IValueConverter
+	{
+		public object NoHeaderAndNoPlaceholderValue { get; set; }
+
+		public object HeaderOnlyValue { get; set; }
+
+		public object HeaderOnlyWithTextValue { get; set; }
+
+		public object PlaceholderOnlyValue { get; set; }
+
+		public object HeaderAndPlaceholderValue { get; set; }
+
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			var textBox = value as TextBox;
+
+			if (textBox is not null)
+			{
+				var noHeader = !(textBox.Header is string str1) || string.IsNullOrEmpty(str1);
+				var noPlaceholder = !(textBox.PlaceholderText is string str2) || string.IsNullOrEmpty(str2);
+				var noText = string.IsNullOrEmpty(textBox.Text);
+
+				if (!noHeader && !noPlaceholder)
+				{
+					return HeaderAndPlaceholderValue;
+				}
+				else if (!noHeader && noPlaceholder)
+				{
+					if (!noText)
+					{
+						return HeaderOnlyWithTextValue;
+					}
+
+					return HeaderOnlyValue;
+				}
+				else if (noHeader && !noPlaceholder)
+				{
+					return PlaceholderOnlyValue;
+				}
+
+				return NoHeaderAndNoPlaceholderValue;
+			}
+
+			return NoHeaderAndNoPlaceholderValue;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			throw new NotSupportedException();
+		}
+	}
+}
