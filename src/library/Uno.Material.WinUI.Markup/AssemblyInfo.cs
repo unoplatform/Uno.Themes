20710ac21de9add031ba2ef4a76cfe1e8f065f92
Nick Randolph <nick@builttoroam.com>
2023-07-31 14:30:37 +1000

    chore: Removing markup from legacy

--- src/library/Uno.Material.WinUI.Markup/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-using Uno.Extensions.Markup.Generator;
-
-[assembly: GenerateMarkupForAssembly(typeof(Uno.Material.MaterialResources))]
-[assembly: AssemblyMetadata("IsTrimmable", "True")]
