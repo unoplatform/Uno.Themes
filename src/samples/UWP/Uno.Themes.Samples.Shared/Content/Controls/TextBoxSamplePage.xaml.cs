f72b01f78f5669ba6acc8bd2625280c7f9ef9547
eriklimakc <erik.lima@knowcode.app>
2023-09-07 17:24:36 +0100

    chore: Adjusting command for icons

--- src/samples/UWP/Uno.Themes.Samples.Shared/Content/Controls/TextBoxSamplePage.xaml.cs
+++ src/samples/UWP/Uno.Themes.Samples.Shared/Content/Controls/TextBoxSamplePage.xaml.cs
@@ -4,7 +4,7 @@ using Windows.UI.Xaml.Controls;
 
 namespace Uno.Themes.Samples.Content.Controls
 {
-	[SamplePage(SampleCategory.Controls, "TextBox", Description = "This control allows users to input a textual value.", DocumentationLink = "https://docs.microsoft.com/en-us/uwp/api/windows.ui.xaml.controls.textbox", DataType = typeof(TestCommands))]
+	[SamplePage(SampleCategory.Controls, "TextBox", Description = "This control allows users to input a textual value.", DocumentationLink = "https://docs.microsoft.com/en-us/uwp/api/windows.ui.xaml.controls.textbox")]
 	public sealed partial class TextBoxSamplePage : Page
 	{
 		public TextBoxSamplePage()
--- src/samples/UWP/Uno.Themes.Samples.Shared/Content/Extensions/ControlExtensionsSamplePage.xaml.cs
+++ src/samples/UWP/Uno.Themes.Samples.Shared/Content/Extensions/ControlExtensionsSamplePage.xaml.cs
@@ -1,15 +1,34 @@
-﻿using Uno.Themes.Samples.Entities;
-using Uno.Themes.Samples.Entities.Data;
+﻿using System;
+using System.Windows.Input;
+using Uno.Themes.Samples.Entities;
+using Uno.Themes.Samples.ViewModels;
 using Windows.UI.Xaml.Controls;
 
 namespace Uno.Themes.Samples.Shared.Content.Extensions
 {
-	[SamplePage(SampleCategory.Helpers, "ControlExtensions", IconPath = Icons.Helpers.ControlExtensions, DataType = typeof(TestCommands))]
+	[SamplePage(SampleCategory.Helpers, "ControlExtensions", IconPath = Icons.Helpers.ControlExtensions, DataType = typeof(ControlExtensionsSampleViewModel))]
 	public sealed partial class ControlExtensionsSamplePage : Page
 	{
 		public ControlExtensionsSamplePage()
 		{
 			this.InitializeComponent();
+		}
+	}
+
+	public class ControlExtensionsSampleViewModel : ViewModelBase
+	{
+		public ICommand ShowWarning => new Command(ShowWarningImpl);
+
+		private async void ShowWarningImpl(object parameter)
+		{
+			var messageDialog = new ContentDialog
+			{
+				Title = "Command Dialog",
+				Content = "A command was fired",
+
+				CloseButtonText = "Ok"
+			};
+			await messageDialog.ShowAsync();
 		}
 	}
 }
