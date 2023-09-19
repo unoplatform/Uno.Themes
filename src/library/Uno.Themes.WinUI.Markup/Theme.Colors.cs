c44c55010cb17742eeb0230e02f96b8e47d25aa0
Dan Siegel <me@dansiegel.net>
2023-09-19 15:12:31 -0600

    chore: update to use ResourceKey from C# Markup

--- src/library/Uno.Themes.WinUI.Markup/Theme.Colors.cs
+++ src/library/Uno.Themes.WinUI.Markup/Theme.Colors.cs
@@ -10,136 +10,136 @@ namespace Uno.Themes.Markup
 			public static class Primary
 			{
 				[ResourceKeyDefinition(typeof(Color), "PrimaryColor")]
-				public static ResourceValue<Color> Default => new("PrimaryColor", true);
+				public static ThemeResourceKey<Color> Default => new("PrimaryColor");
 
 				[ResourceKeyDefinition(typeof(Color), "PrimaryInverseColor")]
-				public static ResourceValue<Color> Inverse => new("PrimaryInverseColor", true);
+				public static ThemeResourceKey<Color> Inverse => new("PrimaryInverseColor");
 
 				[ResourceKeyDefinition(typeof(Color), "PrimaryContainerColor")]
-				public static ResourceValue<Color> Container => new("PrimaryContainerColor", true);
+				public static ThemeResourceKey<Color> Container => new("PrimaryContainerColor");
 
 				[ResourceKeyDefinition(typeof(Color), "PrimaryVariantDarkColor")]
-				public static ResourceValue<Color> VariantDark => new("PrimaryVariantDarkColor", true);
+				public static ThemeResourceKey<Color> VariantDark => new("PrimaryVariantDarkColor");
 
 				[ResourceKeyDefinition(typeof(Color), "PrimaryVariantLightColor")]
-				public static ResourceValue<Color> VariantLight => new("PrimaryVariantLightColor", true);
+				public static ThemeResourceKey<Color> VariantLight => new("PrimaryVariantLightColor");
 			}
 
 			public static class OnPrimary
 			{
 				[ResourceKeyDefinition(typeof(Color), "OnPrimaryColor")]
-				public static ResourceValue<Color> Default => new("OnPrimaryColor", true);
+				public static ThemeResourceKey<Color> Default => new("OnPrimaryColor");
 
 				[ResourceKeyDefinition(typeof(Color), "OnPrimaryContainerColor")]
-				public static ResourceValue<Color> Container => new("OnPrimaryContainerColor", true);
+				public static ThemeResourceKey<Color> Container => new("OnPrimaryContainerColor");
 			}
 
 			public static class Secondary
 			{
 				[ResourceKeyDefinition(typeof(Color), "SecondaryColor")]
-				public static ResourceValue<Color> Default => new("SecondaryColor", true);
+				public static ThemeResourceKey<Color> Default => new("SecondaryColor");
 
 				[ResourceKeyDefinition(typeof(Color), "SecondaryContainerColor")]
-				public static ResourceValue<Color> Container => new("SecondaryContainerColor", true);
+				public static ThemeResourceKey<Color> Container => new("SecondaryContainerColor");
 
 				[ResourceKeyDefinition(typeof(Color), "SecondaryVariantDarkColor")]
-				public static ResourceValue<Color> VariantDark => new("SecondaryVariantDarkColor", true);
+				public static ThemeResourceKey<Color> VariantDark => new("SecondaryVariantDarkColor");
 
 				[ResourceKeyDefinition(typeof(Color), "SecondaryVariantLightColor")]
-				public static ResourceValue<Color> VariantLight => new("SecondaryVariantLightColor", true);
+				public static ThemeResourceKey<Color> VariantLight => new("SecondaryVariantLightColor");
 			}
 
 			public static class OnSecondary
 			{
 				[ResourceKeyDefinition(typeof(Color), "OnSecondaryColor")]
-				public static ResourceValue<Color> Default => new("OnSecondaryColor", true);
+				public static ThemeResourceKey<Color> Default => new("OnSecondaryColor");
 
 				[ResourceKeyDefinition(typeof(Color), "OnSecondaryContainerColor")]
-				public static ResourceValue<Color> Container => new("OnSecondaryContainerColor", true);
+				public static ThemeResourceKey<Color> Container => new("OnSecondaryContainerColor");
 			}
 
 			public static class Tertiary
 			{
 				[ResourceKeyDefinition(typeof(Color), "TertiaryColor")]
-				public static ResourceValue<Color> Default => new("TertiaryColor", true);
+				public static ThemeResourceKey<Color> Default => new("TertiaryColor");
 
 				[ResourceKeyDefinition(typeof(Color), "TertiaryContainerColor")]
-				public static ResourceValue<Color> Container => new("TertiaryContainerColor", true);
+				public static ThemeResourceKey<Color> Container => new("TertiaryContainerColor");
 			}
 
 			public static class OnTertiary
 			{
 				[ResourceKeyDefinition(typeof(Color), "OnTertiaryColor")]
-				public static ResourceValue<Color> Default => new("OnTertiaryColor", true);
+				public static ThemeResourceKey<Color> Default => new("OnTertiaryColor");
 
 				[ResourceKeyDefinition(typeof(Color), "OnTertiaryContainerColor")]
-				public static ResourceValue<Color> Container => new("OnTertiaryContainerColor", true);
+				public static ThemeResourceKey<Color> Container => new("OnTertiaryContainerColor");
 			}
 
 			public static class Error
 			{
 				[ResourceKeyDefinition(typeof(Color), "ErrorColor")]
-				public static ResourceValue<Color> Default => new("ErrorColor", true);
+				public static ThemeResourceKey<Color> Default => new("ErrorColor");
 
 				[ResourceKeyDefinition(typeof(Color), "ErrorContainerColor")]
-				public static ResourceValue<Color> Container => new("ErrorContainerColor", true);
+				public static ThemeResourceKey<Color> Container => new("ErrorContainerColor");
 			}
 
 			public static class OnError
 			{
 				[ResourceKeyDefinition(typeof(Color), "OnErrorColor")]
-				public static ResourceValue<Color> Default => new("OnErrorColor", true);
+				public static ThemeResourceKey<Color> Default => new("OnErrorColor");
 
 				[ResourceKeyDefinition(typeof(Color), "OnErrorContainerColor")]
-				public static ResourceValue<Color> Container => new("OnErrorContainerColor", true);
+				public static ThemeResourceKey<Color> Container => new("OnErrorContainerColor");
 			}
 
 			public static class Background
 			{
 				[ResourceKeyDefinition(typeof(Color), "BackgroundColor")]
-				public static ResourceValue<Color> Default => new("BackgroundColor", true);
+				public static ThemeResourceKey<Color> Default => new("BackgroundColor");
 			}
 
 			public static class OnBackground
 			{
 				[ResourceKeyDefinition(typeof(Color), "OnBackgroundColor")]
-				public static ResourceValue<Color> Default => new("OnBackgroundColor", true);
+				public static ThemeResourceKey<Color> Default => new("OnBackgroundColor");
 			}
 
 			public static class Surface
 			{
 				[ResourceKeyDefinition(typeof(Color), "SurfaceColor")]
-				public static ResourceValue<Color> Default => new("SurfaceColor", true);
+				public static ThemeResourceKey<Color> Default => new("SurfaceColor");
 
 				[ResourceKeyDefinition(typeof(Color), "SurfaceVariantColor")]
-				public static ResourceValue<Color> Variant => new("SurfaceVariantColor", true);
+				public static ThemeResourceKey<Color> Variant => new("SurfaceVariantColor");
 
 				[ResourceKeyDefinition(typeof(Color), "SurfaceInverseColor")]
-				public static ResourceValue<Color> Inverse => new("SurfaceInverseColor", true);
+				public static ThemeResourceKey<Color> Inverse => new("SurfaceInverseColor");
 
 				[ResourceKeyDefinition(typeof(Color), "SurfaceTintColor")]
-				public static ResourceValue<Color> Tint => new("SurfaceTintColor", true);
+				public static ThemeResourceKey<Color> Tint => new("SurfaceTintColor");
 			}
 
 			public static class OnSurface
 			{
 				[ResourceKeyDefinition(typeof(Color), "OnSurfaceColor")]
-				public static ResourceValue<Color> Default => new("OnSurfaceColor", true);
+				public static ThemeResourceKey<Color> Default => new("OnSurfaceColor");
 
 				[ResourceKeyDefinition(typeof(Color), "OnSurfaceVariantColor")]
-				public static ResourceValue<Color> Variant => new("OnSurfaceVariantColor", true);
+				public static ThemeResourceKey<Color> Variant => new("OnSurfaceVariantColor");
 
 				[ResourceKeyDefinition(typeof(Color), "OnSurfaceInverseColor")]
-				public static ResourceValue<Color> Inverse => new("OnSurfaceInverseColor", true);
+				public static ThemeResourceKey<Color> Inverse => new("OnSurfaceInverseColor");
 			}
 
 			public static class Outline
 			{
 				[ResourceKeyDefinition(typeof(Color), "OutlineColor")]
-				public static ResourceValue<Color> Default => new("OutlineColor", true);
+				public static ThemeResourceKey<Color> Default => new("OutlineColor");
 
 				[ResourceKeyDefinition(typeof(Color), "OutlineVariantColor")]
-				public static ResourceValue<Color> Variant => new ("OutlineVariantColor", true);
+				public static ThemeResourceKey<Color> Variant => new ("OutlineVariantColor");
 			}
 
 		}
--- src/library/Uno.Themes.WinUI.Markup/Theme.Styles.cs
+++ src/library/Uno.Themes.WinUI.Markup/Theme.Styles.cs
@@ -10,205 +10,205 @@ namespace Uno.Themes.Markup
 			public static class CalendarDatePicker
 			{
 				[ResourceKeyDefinition(typeof(Style), "CalendarDatePickerStyle", TargetType = typeof(CalendarDatePicker))]
-				public static ResourceValue<Style> Default => new("CalendarDatePickerStyle");
+				public static StaticResourceKey<Style> Default => new("CalendarDatePickerStyle");
 			}
 
 			public static class CalendarView
 			{
 				[ResourceKeyDefinition(typeof(Style), "CalendarViewStyle", TargetType = typeof(CalendarView))]
-				public static ResourceValue<Style> Default => new("CalendarViewStyle");
+				public static StaticResourceKey<Style> Default => new("CalendarViewStyle");
 			}
 
 			public static class CheckBox
 			{
 				[ResourceKeyDefinition(typeof(Style), "CheckBoxStyle", TargetType = typeof(CheckBox))]
-				public static ResourceValue<Style> Default => new("CheckBoxStyle");
+				public static StaticResourceKey<Style> Default => new("CheckBoxStyle");
 
 				[ResourceKeyDefinition(typeof(Style), "SecondaryCheckBoxStyle", TargetType = typeof(CheckBox))]
-				public static ResourceValue<Style> Secondary => new("SecondaryCheckBoxStyle");
+				public static StaticResourceKey<Style> Secondary => new("SecondaryCheckBoxStyle");
 			}
 
 			public static class AppBarButton
 			{
 				[ResourceKeyDefinition(typeof(Style), "AppBarButtonStyle", TargetType = typeof(AppBarButton))]
-				public static ResourceValue<Style> Default => new("AppBarButtonStyle");
+				public static StaticResourceKey<Style> Default => new("AppBarButtonStyle");
 			}
 
 			public static class CommandBar
 			{
 				[ResourceKeyDefinition(typeof(Style), "CommandBarStyle", TargetType = typeof(CommandBar))]
-				public static ResourceValue<Style> Default => new("CommandBarStyle");
+				public static StaticResourceKey<Style> Default => new("CommandBarStyle");
 			}
 
 			public static class ContentDialog
 			{
 				[ResourceKeyDefinition(typeof(Style), "ContentDialogStyle", TargetType = typeof(ContentDialog))]
-				public static ResourceValue<Style> Default => new("ContentDialogStyle");
+				public static StaticResourceKey<Style> Default => new("ContentDialogStyle");
 			}
 
 			public static class DatePicker
 			{
 				[ResourceKeyDefinition(typeof(Style), "DatePickerStyle", TargetType = typeof(DatePicker))]
-				public static ResourceValue<Style> Default => new("DatePickerStyle");
+				public static StaticResourceKey<Style> Default => new("DatePickerStyle");
 			}
 
 			public static class FlyoutPresenter
 			{
 				[ResourceKeyDefinition(typeof(Style), "FlyoutPresenterStyle", TargetType = typeof(FlyoutPresenter))]
-				public static ResourceValue<Style> Default => new("FlyoutPresenterStyle");
+				public static StaticResourceKey<Style> Default => new("FlyoutPresenterStyle");
 			}
 
 			public static class MenuFlyoutItem
 			{
 				[ResourceKeyDefinition(typeof(Style), "MenuFlyoutItemStyle", TargetType = typeof(MenuFlyoutItem))]
-				public static ResourceValue<Style> Default => new("MenuFlyoutItemStyle");
+				public static StaticResourceKey<Style> Default => new("MenuFlyoutItemStyle");
 			}
 
 			public static class MenuFlyoutPresenter
 			{
 				[ResourceKeyDefinition(typeof(Style), "MenuFlyoutPresenterStyle", TargetType = typeof(MenuFlyoutPresenter))]
-				public static ResourceValue<Style> Default => new("MenuFlyoutPresenterStyle");
+				public static StaticResourceKey<Style> Default => new("MenuFlyoutPresenterStyle");
 			}
 
 			public static class HyperlinkButton
 			{
 				[ResourceKeyDefinition(typeof(Style), "HyperlinkButtonStyle", TargetType = typeof(HyperlinkButton))]
-				public static ResourceValue<Style> Default => new("HyperlinkButtonStyle");
+				public static StaticResourceKey<Style> Default => new("HyperlinkButtonStyle");
 
 				[ResourceKeyDefinition(typeof(Style), "SecondaryHyperlinkButtonStyle", TargetType = typeof(HyperlinkButton))]
-				public static ResourceValue<Style> Secondary => new("SecondaryHyperlinkButtonStyle");
+				public static StaticResourceKey<Style> Secondary => new("SecondaryHyperlinkButtonStyle");
 			}
 
 			public static class ListViewItem
 			{
 				[ResourceKeyDefinition(typeof(Style), "ListViewItemStyle", TargetType = typeof(ListViewItem))]
-				public static ResourceValue<Style> Default => new("ListViewItemStyle");
+				public static StaticResourceKey<Style> Default => new("ListViewItemStyle");
 			}
 
 			public static class ListView
 			{
 				[ResourceKeyDefinition(typeof(Style), "ListViewStyle", TargetType = typeof(ListView))]
-				public static ResourceValue<Style> Default => new("ListViewStyle");
+				public static StaticResourceKey<Style> Default => new("ListViewStyle");
 			}
 
 			public static class NavigationView
 			{
 				[ResourceKeyDefinition(typeof(Style), "NavigationViewStyle", TargetType = typeof(NavigationView))]
-				public static ResourceValue<Style> Default => new("NavigationViewStyle");
+				public static StaticResourceKey<Style> Default => new("NavigationViewStyle");
 			}
 
 			public static class NavigationViewItem
 			{
 				[ResourceKeyDefinition(typeof(Style), "NavigationViewItemStyle", TargetType = typeof(NavigationViewItem))]
-				public static ResourceValue<Style> Default => new("NavigationViewItemStyle");
+				public static StaticResourceKey<Style> Default => new("NavigationViewItemStyle");
 			}
 
 			public static class PipsPager
 			{
 				[ResourceKeyDefinition(typeof(Style), "PipsPagerStyle", TargetType = typeof(PipsPager))]
-				public static ResourceValue<Style> Default => new("PipsPagerStyle");
+				public static StaticResourceKey<Style> Default => new("PipsPagerStyle");
 			}
 
 			public static class ProgressBar
 			{
 				[ResourceKeyDefinition(typeof(Style), "ProgressBarStyle", TargetType = typeof(ProgressBar))]
-				public static ResourceValue<Style> Default => new("ProgressBarStyle");
+				public static StaticResourceKey<Style> Default => new("ProgressBarStyle");
 			}
 
 			public static class ProgressRing
 			{
 				[ResourceKeyDefinition(typeof(Style), "ProgressRingStyle", TargetType = typeof(ProgressRing))]
-				public static ResourceValue<Style> Default => new("ProgressRingStyle");
+				public static StaticResourceKey<Style> Default => new("ProgressRingStyle");
 			}
 
 			public static class Slider
 			{
 				[ResourceKeyDefinition(typeof(Style), "SliderStyle", TargetType = typeof(Slider))]
-				public static ResourceValue<Style> Default => new("SliderStyle");
+				public static StaticResourceKey<Style> Default => new("SliderStyle");
 			}
 
 			public static class TextBlock
 			{
 				[ResourceKeyDefinition(typeof(Style), "DisplayLarge", TargetType = typeof(TextBlock))]
-				public static ResourceValue<Style> DisplayLarge => new("DisplayLarge");
+				public static StaticResourceKey<Style> DisplayLarge => new("DisplayLarge");
 
 				[ResourceKeyDefinition(typeof(Style), "DisplayMedium", TargetType = typeof(TextBlock))]
-				public static ResourceValue<Style> DisplayMedium => new("DisplayMedium");
+				public static StaticResourceKey<Style> DisplayMedium => new("DisplayMedium");
 
 				[ResourceKeyDefinition(typeof(Style), "DisplaySmall", TargetType = typeof(TextBlock))]
-				public static ResourceValue<Style> DisplaySmall => new("DisplaySmall");
+				public static StaticResourceKey<Style> DisplaySmall => new("DisplaySmall");
 
 				[ResourceKeyDefinition(typeof(Style), "HeadlineLarge", TargetType = typeof(TextBlock))]
-				public static ResourceValue<Style> HeadlineLarge => new("HeadlineLarge");
+				public static StaticResourceKey<Style> HeadlineLarge => new("HeadlineLarge");
 
 				[ResourceKeyDefinition(typeof(Style), "HeadlineMedium", TargetType = typeof(TextBlock))]
-				public static ResourceValue<Style> HeadlineMedium => new("HeadlineMedium");
+				public static StaticResourceKey<Style> HeadlineMedium => new("HeadlineMedium");
 
 				[ResourceKeyDefinition(typeof(Style), "HeadlineSmall", TargetType = typeof(TextBlock))]
-				public static ResourceValue<Style> HeadlineSmall => new("HeadlineSmall");
+				public static StaticResourceKey<Style> HeadlineSmall => new("HeadlineSmall");
 
 				[ResourceKeyDefinition(typeof(Style), "TitleLarge", TargetType = typeof(TextBlock))]
-				public static ResourceValue<Style> TitleLarge => new("TitleLarge");
+				public static StaticResourceKey<Style> TitleLarge => new("TitleLarge");
 
 				[ResourceKeyDefinition(typeof(Style), "TitleMedium", TargetType = typeof(TextBlock))]
-				public static ResourceValue<Style> TitleMedium => new("TitleMedium");
+				public static StaticResourceKey<Style> TitleMedium => new("TitleMedium");
 
 				[ResourceKeyDefinition(typeof(Style), "TitleSmall", TargetType = typeof(TextBlock))]
-				public static ResourceValue<Style> TitleSmall => new("TitleSmall");
+				public static StaticResourceKey<Style> TitleSmall => new("TitleSmall");
 
 				[ResourceKeyDefinition(typeof(Style), "LabelLarge", TargetType = typeof(TextBlock))]
-				public static ResourceValue<Style> LabelLarge => new("LabelLarge");
+				public static StaticResourceKey<Style> LabelLarge => new("LabelLarge");
 
 				[ResourceKeyDefinition(typeof(Style), "LabelMedium", TargetType = typeof(TextBlock))]
-				public static ResourceValue<Style> LabelMedium => new("LabelMedium");
+				public static StaticResourceKey<Style> LabelMedium => new("LabelMedium");
 
 				[ResourceKeyDefinition(typeof(Style), "LabelSmall", TargetType = typeof(TextBlock))]
-				public static ResourceValue<Style> LabelSmall => new("LabelSmall");
+				public static StaticResourceKey<Style> LabelSmall => new("LabelSmall");
 
 				[ResourceKeyDefinition(typeof(Style), "LabelExtraSmall", TargetType = typeof(TextBlock))]
-				public static ResourceValue<Style> LabelExtraSmall => new("LabelExtraSmall");
+				public static StaticResourceKey<Style> LabelExtraSmall => new("LabelExtraSmall");
 
 				[ResourceKeyDefinition(typeof(Style), "BodyLarge", TargetType = typeof(TextBlock))]
-				public static ResourceValue<Style> BodyLarge => new("BodyLarge");
+				public static StaticResourceKey<Style> BodyLarge => new("BodyLarge");
 
 				[ResourceKeyDefinition(typeof(Style), "BodyMedium", TargetType = typeof(TextBlock))]
-				public static ResourceValue<Style> BodyMedium => new("BodyMedium");
+				public static StaticResourceKey<Style> BodyMedium => new("BodyMedium");
 
 				[ResourceKeyDefinition(typeof(Style), "BodySmall", TargetType = typeof(TextBlock))]
-				public static ResourceValue<Style> BodySmall => new("BodySmall");
+				public static StaticResourceKey<Style> BodySmall => new("BodySmall");
 
 				[ResourceKeyDefinition(typeof(Style), "CaptionLarge", TargetType = typeof(TextBlock))]
-				public static ResourceValue<Style> CaptionLarge => new("CaptionLarge");
+				public static StaticResourceKey<Style> CaptionLarge => new("CaptionLarge");
 
 				[ResourceKeyDefinition(typeof(Style), "CaptionMedium", TargetType = typeof(TextBlock))]
-				public static ResourceValue<Style> CaptionMedium => new("CaptionMedium");
+				public static StaticResourceKey<Style> CaptionMedium => new("CaptionMedium");
 
 				[ResourceKeyDefinition(typeof(Style), "CaptionSmall", TargetType = typeof(TextBlock))]
-				public static ResourceValue<Style> CaptionSmall => new("CaptionSmall");
+				public static StaticResourceKey<Style> CaptionSmall => new("CaptionSmall");
 			}
 
 			public static class TextBox
 			{
 				[ResourceKeyDefinition(typeof(Style), "FilledTextBoxStyle", TargetType = typeof(TextBox))]
-				public static ResourceValue<Style> Filled => new("FilledTextBoxStyle");
+				public static StaticResourceKey<Style> Filled => new("FilledTextBoxStyle");
 
 				[ResourceKeyDefinition(typeof(Style), "OutlinedTextBoxStyle", TargetType = typeof(TextBox))]
-				public static ResourceValue<Style> Outlined => new("OutlinedTextBoxStyle");
+				public static StaticResourceKey<Style> Outlined => new("OutlinedTextBoxStyle");
 			}
 
 			public static class ToggleButton
 			{
 				[ResourceKeyDefinition(typeof(Style), "TextToggleButtonStyle", TargetType = typeof(ToggleButton))]
-				public static ResourceValue<Style> Text => new("TextToggleButtonStyle");
+				public static StaticResourceKey<Style> Text => new("TextToggleButtonStyle");
 
 				[ResourceKeyDefinition(typeof(Style), "IconToggleButtonStyle", TargetType = typeof(ToggleButton))]
-				public static ResourceValue<Style> Icon => new("IconToggleButtonStyle");
+				public static StaticResourceKey<Style> Icon => new("IconToggleButtonStyle");
 			}
 
 			public static class ToggleSwitch
 			{
 				[ResourceKeyDefinition(typeof(Style), "ToggleSwitchStyle", TargetType = typeof(ToggleSwitch))]
-				public static ResourceValue<Style> Default => new("ToggleSwitchStyle");
+				public static StaticResourceKey<Style> Default => new("ToggleSwitchStyle");
 			}
 		}
 	}
