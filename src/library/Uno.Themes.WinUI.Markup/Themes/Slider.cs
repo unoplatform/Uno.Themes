536b568362a9596f86eb2b5e2e3a75f45e856d7a
Lester Botello <lunarian.software@outlook.com>
2023-09-28 11:38:09 -0400

    feat(Slider): Lightweight styling for Material Slider (#1064)
    Co-authored-by: Steve Bilogan <steve.bilogan@gmail.com>

---
+++ src/library/Uno.Themes.WinUI.Markup/Themes/Slider.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Media;
+using Uno.Extensions.Markup;
+using Uno.Extensions.Markup.Internals;
+
+namespace Uno.Themes.Markup
+{
+	public static partial class Theme
+	{
+		public static class Slider
+		{
+			public static class Resources
+			{
+				public static class Typography
+				{
+					[ResourceKeyDefinition(typeof(FontFamily), "SliderFontFamily")]
+					public static ThemeResourceKey<FontFamily> FontFamily => new("SliderFontFamily");
+
+					[ResourceKeyDefinition(typeof(double), "SliderFontSize")]
+					public static ThemeResourceKey<double> FontSize => new("SliderFontSize");
+				}
+
+				public static class Track
+				{
+					public static class Fill
+					{
+						[ResourceKeyDefinition(typeof(Brush), "SliderTrackFill")]
+						public static ThemeResourceKey<Brush> Default => new("SliderTrackFill");
+
+						[ResourceKeyDefinition(typeof(Brush), "SliderTrackFillDisabled")]
+						public static ThemeResourceKey<Brush> Disabled => new("SliderTrackFillDisabled");
+					}
+
+					public static class ValueFill
+					{
+						[ResourceKeyDefinition(typeof(Brush), "SliderTrackFill")]
+						public static ThemeResourceKey<Brush> Default => new("SliderTrackFill");
+
+						[ResourceKeyDefinition(typeof(Brush), "SliderTrackFillDisabled")]
+						public static ThemeResourceKey<Brush> Disabled => new("SliderTrackFillDisabled");
+					}
+
+					[ResourceKeyDefinition(typeof(CornerRadius), "SliderTrackCornerRadius")]
+					public static ThemeResourceKey<CornerRadius> CornerRadius => new("SliderTrackCornerRadius");
+				}
+
+				public static class TickBar
+				{
+					public static class Fill
+					{
+						[ResourceKeyDefinition(typeof(Brush), "SliderTickBarFill")]
+						public static ThemeResourceKey<Brush> Default => new("SliderTickBarFill");
+
+						[ResourceKeyDefinition(typeof(Brush), "SliderTickBarFillDisabled")]
+						public static ThemeResourceKey<Brush> Disabled => new("SliderTickBarFillDisabled");
+					}
+
+					[ResourceKeyDefinition(typeof(Brush), "SliderInlineTickBarFill")]
+					public static ThemeResourceKey<Brush> InlineFill => new("SliderInlineTickBarFill");
+				}
+
+				public static class Thumb
+				{
+					public static class Background
+					{
+						[ResourceKeyDefinition(typeof(Brush), "SliderThumbBackground")]
+						public static ThemeResourceKey<Brush> Default => new("SliderThumbBackground");
+
+						[ResourceKeyDefinition(typeof(Brush), "SliderThumbBackgroundDisabled")]
+						public static ThemeResourceKey<Brush> Disabled => new("SliderThumbBackgroundDisabled");
+					}
+				}
+			}
+
+			public static class Styles
+			{
+				[ResourceKeyDefinition(typeof(Style), "MaterialSliderStyle", TargetType = typeof(Slider))]
+				public static StaticResourceKey<Style> Default => new("MaterialSliderStyle");
+
+				public static class Thumb
+				{
+					[ResourceKeyDefinition(typeof(Style), "MaterialSliderThumbStyle", TargetType = typeof(Microsoft.UI.Xaml.Controls.Primitives.Thumb))]
+					public static ThemeResourceKey<Style> Default => new("MaterialSliderThumbStyle");
+				}
+			}
+		}
+	}
+}
