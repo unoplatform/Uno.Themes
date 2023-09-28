70747a97ee9d84c70a23d28ee53ee5505ad78d02
Matthew Rajala <70542961+rajamatt@users.noreply.github.com>
2023-09-28 11:32:23 -0400

    feat(ProgressBar-ProgressRing): lightweight styling for ProgressBar-Ring (#1058)
    Co-authored-by: Ariel De Los Santos <ariel.delossantos@nventive.com>

---
+++ src/library/Uno.Themes.WinUI.Markup/Theme/ProgressBar.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Media;
+using Uno.Extensions.Markup;
+using Uno.Extensions.Markup.Internals;
+
+namespace Uno.Themes.Markup
+{
+	public static partial class Theme
+	{
+		public static class ProgressBar
+		{
+			public static class Resources
+			{
+				[ResourceKeyDefinition(typeof(Brush), "ProgressBarForeground")]
+				public static ThemeResourceKey<Brush> Foreground => new("ProgressBarForeground");
+
+				[ResourceKeyDefinition(typeof(Brush), "ProgressBarBackground")]
+				public static ThemeResourceKey<Brush> Background => new("ProgressBarBackground");
+
+				[ResourceKeyDefinition(typeof(double), "ProgressBarMinWidth")]
+				public static ThemeResourceKey<double> MinWidth => new("ProgressBarMinWidth");
+
+				[ResourceKeyDefinition(typeof(double), "ProgressBarHeight")]
+				public static ThemeResourceKey<double> Height => new("ProgressBarHeight");
+			}
+
+			public static class Styles
+			{
+				[ResourceKeyDefinition(typeof(Style), "ProgressBarStyle", TargetType = typeof(ProgressBar))]
+				public static StaticResourceKey<Style> Default => new("ProgressBarStyle");
+			}
+		}
+	}
+}
---
+++ src/library/Uno.Themes.WinUI.Markup/Theme/ProgressRing.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Media;
+using Uno.Extensions.Markup;
+using Uno.Extensions.Markup.Internals;
+
+namespace Uno.Themes.Markup
+{
+	public static partial class Theme
+	{
+		public static class ProgressRing
+		{
+			public static class Resources
+			{
+				public static class Foreground
+				{
+					[ResourceKeyDefinition(typeof(Brush), "ProgressRingForeground")]
+					public static ThemeResourceKey<Brush> Default => new("ProgressRingForeground");
+				}
+
+				public static class Background
+				{
+					[ResourceKeyDefinition(typeof(Brush), "ProgressRingBackground")]
+					public static ThemeResourceKey<Brush> Default => new("ProgressRingBackground");
+				}
+			}
+
+			public static class Styles
+			{
+				[ResourceKeyDefinition(typeof(Style), "ProgressRingStyle", TargetType = typeof(ProgressRing))]
+				public static StaticResourceKey<Style> Default => new("ProgressRingStyle");
+			}
+		}
+	}
+}
