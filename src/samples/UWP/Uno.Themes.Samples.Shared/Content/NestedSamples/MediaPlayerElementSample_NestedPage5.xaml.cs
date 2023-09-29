6bebcc06647e6aac5c14cd58c6e38809eeae8ad9
Vy-Phuong Nguyen <54756963+nguyeenvy@users.noreply.github.com>
2023-09-29 12:18:53 -0400

    feat: Add MediaPlayerElement styles and sample pages (#1042)

---
+++ src/samples/UWP/Uno.Themes.Samples.Shared/Content/NestedSamples/MediaPlayerElementSample_NestedPage5.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace Uno.Themes.Samples.Content.NestedSamples;
+
+public sealed partial class MediaPlayerElementSample_NestedPage5 : Page
+{
+    public MediaPlayerElementSample_NestedPage5()
+    {
+        this.InitializeComponent();
+	InitializePlaybackList();
+	Unloaded += MediaPlayerElementSample_NestedPage5_Unloaded;
+}
+
+private void InitializePlaybackList()
+{
+	var mediaPlaybackList = new MediaPlaybackList();
+
+	mediaPlaybackList.Items.Add(new MediaPlaybackItem(MediaSource.CreateFromUri(new Uri("https://uno-assets.platform.uno/tests/videos/Mobile_Development_in_VS_Code_with_Uno_Platform_orDotNetMAUI.mp4"))));
+	mediaPlaybackList.Items.Add(new MediaPlaybackItem(MediaSource.CreateFromUri(new Uri("https://uno-assets.platform.uno/tests/audio/Getting_Started_with_Uno_Platform_and_Visual_Studio_Code.mp3"))));
+	mediaPlaybackList.Items.Add(new MediaPlaybackItem(MediaSource.CreateFromUri(new Uri("https://uno-assets.platform.uno/tests/videos/Getting_Started_with_Uno_Platform_and_Visual_Studio_Code.mp4"))));
+
+	MediaPlayerElementSample5.MediaPlayer.Source = mediaPlaybackList;
+}
+
+private void NavigateBack(object sender, RoutedEventArgs e) => Shell.GetForCurrentView().BackNavigateFromNestedSample();
+
+private void MediaPlayerElementSample_NestedPage5_Unloaded(object sender, RoutedEventArgs e)
+{
+	MediaPlayerElementSample5.MediaPlayer.Pause();
+}
+}
