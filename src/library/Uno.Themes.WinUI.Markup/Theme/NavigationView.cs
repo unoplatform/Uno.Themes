094b20512c069b134a09cd9ec047038708d80423
Steve Bilogan <steve.bilogan@gmail.com>
2023-10-27 11:10:46 -0400

    chore: whitespace

--- src/library/Uno.Themes.WinUI.Markup/Theme/NavigationView.cs
+++ src/library/Uno.Themes.WinUI.Markup/Theme/NavigationView.cs
@@ -10,85 +10,85 @@ public static partial class Theme
 	public static partial class NavigationView
 	{
 		public static partial class Resources
+		{
+			public static partial class Default
 			{
-				public static partial class Default
+				public static partial class Background
+				{
+					[ResourceKeyDefinition(typeof(Brush), "NavigationViewBackground")]
+					public static ThemeResourceKey<Brush> Default => new("NavigationViewBackground");
+				}
+
+				public static partial class Pane
 				{
 					public static partial class Background
 					{
-						[ResourceKeyDefinition(typeof(Brush), "NavigationViewBackground")]
-						public static ThemeResourceKey<Brush> Default => new("NavigationViewBackground");
+						[ResourceKeyDefinition(typeof(Brush), "NavigationViewDefaultPaneBackground")]
+						public static ThemeResourceKey<Brush> Default => new("NavigationViewDefaultPaneBackground");
+
+						[ResourceKeyDefinition(typeof(Brush), "NavigationViewPaneBackground")]
+						public static ThemeResourceKey<Brush> Normal => new("NavigationViewPaneBackground");
+
+						[ResourceKeyDefinition(typeof(Brush), "NavigationViewTopPaneBackground")]
+						public static ThemeResourceKey<Brush> Top => new("NavigationViewTopPaneBackground");
 					}
 
-					public static partial class Pane
+					public static partial class BorderBrush
 					{
-						public static partial class Background
-						{
-							[ResourceKeyDefinition(typeof(Brush), "NavigationViewDefaultPaneBackground")]
-							public static ThemeResourceKey<Brush> Default => new("NavigationViewDefaultPaneBackground");
+						[ResourceKeyDefinition(typeof(Brush), "NavigationViewDefaultPaneBorderBrush")]
+						public static ThemeResourceKey<Brush> Default => new("NavigationViewDefaultPaneBorderBrush");
+					}
+				}
 
-							[ResourceKeyDefinition(typeof(Brush), "NavigationViewPaneBackground")]
-							public static ThemeResourceKey<Brush> Normal => new("NavigationViewPaneBackground");
+				public static partial class Button
+				{
+					public static partial class Foreground
+					{
+						[ResourceKeyDefinition(typeof(Brush), "NavigationViewButtonForeground")]
+						public static ThemeResourceKey<Brush> Default => new("NavigationViewButtonForeground");
 
-							[ResourceKeyDefinition(typeof(Brush), "NavigationViewTopPaneBackground")]
-							public static ThemeResourceKey<Brush> Top => new("NavigationViewTopPaneBackground");
-						}
+						[ResourceKeyDefinition(typeof(Brush), "NavigationViewButtonForegroundPointerOver")]
+						public static ThemeResourceKey<Brush> PointerOver => new("NavigationViewButtonForegroundPointerOver");
 
-						public static partial class BorderBrush
-						{
-							[ResourceKeyDefinition(typeof(Brush), "NavigationViewDefaultPaneBorderBrush")]
-							public static ThemeResourceKey<Brush> Default => new("NavigationViewDefaultPaneBorderBrush");
-						}
+						[ResourceKeyDefinition(typeof(Brush), "NavigationViewButtonForegroundPressed")]
+						public static ThemeResourceKey<Brush> Pressed => new("NavigationViewButtonForegroundPressed");
+
+						[ResourceKeyDefinition(typeof(Brush), "NavigationViewButtonForegroundDisabled")]
+						public static ThemeResourceKey<Brush> Disabled => new("NavigationViewButtonForegroundDisabled");
 					}
 
-					public static partial class Button
+					public static partial class Background
 					{
-						public static partial class Foreground
-						{
-							[ResourceKeyDefinition(typeof(Brush), "NavigationViewButtonForeground")]
-							public static ThemeResourceKey<Brush> Default => new("NavigationViewButtonForeground");
+						[ResourceKeyDefinition(typeof(Brush), "NavigationViewButtonBackground")]
+						public static ThemeResourceKey<Brush> Default => new("NavigationViewButtonBackground");
 
-							[ResourceKeyDefinition(typeof(Brush), "NavigationViewButtonForegroundPointerOver")]
-							public static ThemeResourceKey<Brush> PointerOver => new("NavigationViewButtonForegroundPointerOver");
+						[ResourceKeyDefinition(typeof(Brush), "NavigationViewButtonBackgroundPointerOver")]
+						public static ThemeResourceKey<Brush> PointerOver => new("NavigationViewButtonBackgroundPointerOver");
 
-							[ResourceKeyDefinition(typeof(Brush), "NavigationViewButtonForegroundPressed")]
-							public static ThemeResourceKey<Brush> Pressed => new("NavigationViewButtonForegroundPressed");
+						[ResourceKeyDefinition(typeof(Brush), "NavigationViewButtonBackgroundPressed")]
+						public static ThemeResourceKey<Brush> Pressed => new("NavigationViewButtonBackgroundPressed");
 
-							[ResourceKeyDefinition(typeof(Brush), "NavigationViewButtonForegroundDisabled")]
-							public static ThemeResourceKey<Brush> Disabled => new("NavigationViewButtonForegroundDisabled");
-						}
-
-						public static partial class Background
-						{
-							[ResourceKeyDefinition(typeof(Brush), "NavigationViewButtonBackground")]
-							public static ThemeResourceKey<Brush> Default => new("NavigationViewButtonBackground");
-
-							[ResourceKeyDefinition(typeof(Brush), "NavigationViewButtonBackgroundPointerOver")]
-							public static ThemeResourceKey<Brush> PointerOver => new("NavigationViewButtonBackgroundPointerOver");
-
-							[ResourceKeyDefinition(typeof(Brush), "NavigationViewButtonBackgroundPressed")]
-							public static ThemeResourceKey<Brush> Pressed => new("NavigationViewButtonBackgroundPressed");
-
-							[ResourceKeyDefinition(typeof(Brush), "NavigationViewButtonBackgroundDisabled")]
-							public static ThemeResourceKey<Brush> Disabled => new("NavigationViewButtonBackgroundDisabled");
-						}
-
-						public static partial class RippleFeedback
-						{
-							[ResourceKeyDefinition(typeof(Brush), "NavigationViewButtonRippleFeedback")]
-							public static ThemeResourceKey<Brush> Default => new("NavigationViewButtonRippleFeedback");
-						}
+						[ResourceKeyDefinition(typeof(Brush), "NavigationViewButtonBackgroundDisabled")]
+						public static ThemeResourceKey<Brush> Disabled => new("NavigationViewButtonBackgroundDisabled");
 					}
 
-					public static partial class SelectionIndicator
+					public static partial class RippleFeedback
 					{
-						public static partial class Foreground
-						{
-							[ResourceKeyDefinition(typeof(Brush), "NavigationViewSelectionIndicatorForeground")]
-							public static ThemeResourceKey<Brush> Default => new("NavigationViewSelectionIndicatorForeground");
-						}
+						[ResourceKeyDefinition(typeof(Brush), "NavigationViewButtonRippleFeedback")]
+						public static ThemeResourceKey<Brush> Default => new("NavigationViewButtonRippleFeedback");
+					}
+				}
+
+				public static partial class SelectionIndicator
+				{
+					public static partial class Foreground
+					{
+						[ResourceKeyDefinition(typeof(Brush), "NavigationViewSelectionIndicatorForeground")]
+						public static ThemeResourceKey<Brush> Default => new("NavigationViewSelectionIndicatorForeground");
 					}
 				}
 			}
+		}
 		public static partial class Styles
 		{
 			[ResourceKeyDefinition(typeof(Style), "NavigationViewStyle", TargetType = typeof(NavigationView))]
