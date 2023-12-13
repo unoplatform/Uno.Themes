3c6ae86223b51953b0e7e25c64f8d0f8f4d1267b
Pedro Jesus <pedrojesus.cefet@gmail.com>
2023-12-13 19:37:09 -0300

    chore: moved Material.Converters to Uno.Themes

---
+++ src/library/Uno.Themes/Converters/StringFormatConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+#if WinUI
+using Microsoft.UI.Xaml.Data;
+#else
+using Windows.UI.Xaml.Data;
+#endif
+
+namespace Uno.Themes;
+
+public class StringFormatConverter : IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, string language)
+	{
+		var format = parameter as string;
+		if (!String.IsNullOrEmpty(format))
+			return String.Format(format, value);
+
+		return value;
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, string language)
+	{
+		throw new NotImplementedException();
+	}
+}
