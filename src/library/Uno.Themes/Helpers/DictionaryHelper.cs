cb7c8a08b5f9d354083dc1fbaefbb937dece1d6b
Pedro Jesus <pedrojesus.cefet@gmail.com>
2023-12-13 18:32:10 -0300

    chore: moved helper files to Uno.Theme

---
+++ src/library/Uno.Themes/Helpers/DictionaryHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+#if WinUI
+using Microsoft.UI.Xaml;
+#else
+using Windows.UI.Xaml;
+#endif
+
+namespace Uno.Themes.Helpers;
+
+internal class DictionaryHelper
+{
+	internal static object CreateLazyResource(Func<Style> styleSelector)
+	{
+#if HAS_UNO
+		return new ResourceDictionary.ResourceInitializer(styleSelector);
+#else
+		return styleSelector();
+#endif
+	}
+}
