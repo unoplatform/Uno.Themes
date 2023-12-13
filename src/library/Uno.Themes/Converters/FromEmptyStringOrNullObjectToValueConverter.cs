8b7da00da1878a86ea57e5a04977441b7c577717
Pedro Jesus <pedrojesus.cefet@gmail.com>
2023-12-13 19:37:09 -0300

    chore!: moved Material.Converters to Uno.Themes
    BREAKING CHANGE: Files moved around

---
+++ src/library/Uno.Themes/Converters/FromEmptyStringOrNullObjectToValueConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+#if WinUI
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+#else
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+#endif
+
+namespace Uno.Themes;
+
+public class FromEmptyStringOrNullObjectToValueConverter : IValueConverter
+{
+	public object EmptyOrNullValue { get; set; }
+
+	public object NotEmptyOrNullValue { get; set; }
+
+	public object Convert(object value, Type targetType, object parameter, string language)
+	{
+		if (value is string str && string.IsNullOrEmpty(str))
+		{
+			return EmptyOrNullValue;
+		}
+
+		if (value == null || value == DependencyProperty.UnsetValue)
+		{
+			return EmptyOrNullValue;
+		}
+
+		return NotEmptyOrNullValue;
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, string language)
+	{
+		throw new NotSupportedException();
+	}
+}
---
+++ src/library/Uno.Themes/Converters/FromOpenToCustomValueConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+#if WinUI
+using Microsoft.UI.Xaml.Data;
+#else
+using Windows.UI.Xaml.Data;
+#endif
+
+namespace Uno.Themes;
+
+public class FromOpenToCustomValueConverter : IValueConverter
+{
+	public object ValueIfOpen { get; set; }
+
+	public object ValueIfClosed { get; set; }
+
+	public object Convert(object value, Type targetType, object parameter, string language)
+	{
+		if ((bool)value)
+		{
+			return ValueIfOpen;
+		}
+
+		return ValueIfClosed;
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, string language)
+	{
+		throw new NotSupportedException();
+	}
+}
