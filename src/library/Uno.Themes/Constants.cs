62c5f567973227359936c1bdca249c14cbb207de
Pedro Jesus <pedrojesus.cefet@gmail.com>
2023-12-20 17:42:03 -0300

    chore: used a constant file for path

---
+++ src/library/Uno.Themes/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: InternalsVisibleTo("Uno.Cupertino")]
+[assembly: InternalsVisibleTo("Uno.Material")]
+namespace Uno.Themes;
+
+internal static class Constants
+{
+	public const string ConverterResourcePath = "ms-appx:///Uno.Themes/Styles/Applications/Common/Converters.xaml";
+}
