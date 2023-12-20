84c43ec9ae354afd5d54dea4f93e3e9a35bfe370
Pedro Jesus <pedrojesus.cefet@gmail.com>
2023-12-20 17:16:19 -0300

    chore: fixed wrong name

--- src/library/Uno.Material/MaterialResourcesV1.cs
+++ src/library/Uno.Material/MaterialResourcesV1.cs
@@ -17,7 +17,7 @@ namespace Uno.Material
 	{
 		public MaterialResourcesV1()
 		{
-			MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///Uno.Themes/Styles/Applications/Commnon/Converters.xaml") });
+			MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///Uno.Themes/Styles/Applications/Common/Converters.xaml") });
 			Source = new Uri("ms-appx:///Uno.Material/Generated/mergedpages.v1.xaml");
 		}
 
