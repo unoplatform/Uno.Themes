2116041ca1e8fc836745253bfe42ba7faaa795ab
Steve Bilogan <steve.bilogan@gmail.com>
2024-01-03 12:35:57 -0500

    wip

---
+++ src/samples/Uno.Themes.Samples.Shared/App.xaml.cs
@@ -0,0 +1,168 @@
+﻿using Microsoft.Extensions.Logging;
+using ShowMeTheXAML;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Uno.Extensions;
+using Uno.Themes.Samples.Content.Styles;
+using Uno.Themes.Samples.Entities;
+using Uno.Themes.Samples.Helpers;
+
+#if IS_WINUI
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using XamlLaunchActivatedEventArgs = Microsoft.UI.Xaml.LaunchActivatedEventArgs;
+using XamlWindow = Microsoft.UI.Xaml.Window;
+using Page = Microsoft.UI.Xaml.Controls.Page;
+#else
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using XamlLaunchActivatedEventArgs = Windows.ApplicationModel.Activation.LaunchActivatedEventArgs;
+using XamlWindow = Windows.UI.Xaml.Window;
+using Page = Windows.UI.Xaml.Controls.Page;
+#endif
+
+namespace Uno.Themes.Samples
+{
+	/// <summary>
+	/// Provides application-specific behavior to supplement the default Application class.
+	/// </summary>
+	public partial class App : Application
+	{
+
+		private Shell _shell;
+
+		/// <summary>
+		/// Initializes the singleton application object.  This is the first line of authored code
+		/// executed, and as such is the logical equivalent of main() or WinMain().
+		/// </summary>
+		public App()
+		{
+			InitializeLogging();
+
+			ConfigureXamlDisplay();
+
+			this.InitializeComponent();
+			this.Suspending += OnSuspending;
+		}
+
+		/// <summary>
+		/// Invoked when the application is launched normally by the end user.  Other entry points
+		/// will be used such as when the application is launched to open a specific file.
+		/// </summary>
+		/// <param name="e">Details about the launch request and process.</param>
+		protected override void OnLaunched(XamlLaunchActivatedEventArgs e)
+		{
+#if DEBUG
+			if (System.Diagnostics.Debugger.IsAttached)
+			{
+				// this.DebugSettings.EnableFrameRateCounter = true;
+			}
+#endif
+
+#if WINDOWS_UWP
+			ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(320, 568)); // (size of the iPhone SE)
+#endif
+
+			var window = XamlWindow.Current;
+			if (!(window.Content is Shell))
+			{
+				window.Content = _shell = BuildShell();
+			}
+
+			// Ensure the current window is active
+			window.Activate();
+		}
+
+
+
+		/// <summary>
+		/// Invoked when application execution is being suspended.  Application state is saved
+		/// without knowing whether the application will be terminated or resumed with the contents
+		/// of memory still intact.
+		/// </summary>
+		/// <param name="sender">The source of the suspend request.</param>
+		/// <param name="e">Details about the suspend request.</param>
+		private void OnSuspending(object sender, SuspendingEventArgs e)
+		{
+			var deferral = e.SuspendingOperation.GetDeferral();
+			//TODO: Save application state and stop any background activity
+			deferral.Complete();
+		}
+
+		/// <summary>
+		/// Configures global Uno Platform logging
+		/// </summary>
+		private static void InitializeLogging()
+		{
+			var factory = LoggerFactory.Create(builder =>
+			{
+#if __WASM__
+				builder.AddProvider(new global::Uno.Extensions.Logging.WebAssembly.WebAssemblyConsoleLoggerProvider());
+#elif __IOS__
+				builder.AddProvider(new global::Uno.Extensions.Logging.OSLogLoggerProvider());
+#elif NETFX_CORE
+				builder.AddDebug();
+#else
+				builder.AddConsole();
+#endif
+
+				// Exclude logs below this level
+				builder.SetMinimumLevel(LogLevel.Information);
+
+				// Default filters for Uno Platform namespaces
+				builder.AddFilter("Uno", LogLevel.Warning);
+				builder.AddFilter("Windows", LogLevel.Warning);
+				builder.AddFilter("Microsoft", LogLevel.Warning);
+
+				// Generic Xaml events
+				// builder.AddFilter("Windows.UI.Xaml", LogLevel.Debug );
+				// builder.AddFilter("Windows.UI.Xaml.VisualStateGroup", LogLevel.Debug );
+				// builder.AddFilter("Windows.UI.Xaml.StateTriggerBase", LogLevel.Debug );
+				// builder.AddFilter("Windows.UI.Xaml.UIElement", LogLevel.Debug );
+				// builder.AddFilter("Windows.UI.Xaml.FrameworkElement", LogLevel.Trace );
+
+				// Layouter specific messages
+				// builder.AddFilter("Windows.UI.Xaml.Controls", LogLevel.Debug );
+				// builder.AddFilter("Windows.UI.Xaml.Controls.Layouter", LogLevel.Debug );
+				// builder.AddFilter("Windows.UI.Xaml.Controls.Panel", LogLevel.Debug );
+
+				// builder.AddFilter("Windows.Storage", LogLevel.Debug );
+
+				// Binding related messages
+				// builder.AddFilter("Windows.UI.Xaml.Data", LogLevel.Debug );
+				// builder.AddFilter("Windows.UI.Xaml.Data", LogLevel.Debug );
+
+				// Binder memory references tracking
+				// builder.AddFilter("Uno.UI.DataBinding.BinderReferenceHolder", LogLevel.Debug );
+
+				// RemoteControl and HotReload related
+				// builder.AddFilter("Uno.UI.RemoteControl", LogLevel.Information);
+
+				// Debug JS interop
+				// builder.AddFilter("Uno.Foundation.WebAssemblyRuntime", LogLevel.Debug );
+			});
+
+			global::Uno.Extensions.LogExtensionPoint.AmbientLoggerFactory = factory;
+		}
+
+		static void ConfigureXamlDisplay()
+		{
+			XamlDisplay.Init();
+		}
+	}
+}
