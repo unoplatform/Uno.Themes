0e69144c4eb34e20c7f6a8d657bcfa881167d523
Pedro Jesus <pedrojesus.cefet@gmail.com>
2024-01-15 16:13:20 -0300

    chore: changed the markup generation to WinUI.Markup project

--- src/library/Uno.Material.WinUI.Markup/AssemblyInfo.cs
+++ src/library/Uno.Material.WinUI.Markup/AssemblyInfo.cs
@@ -2,5 +2,4 @@
 using Uno.Extensions.Markup.Generator;
 
 [assembly: GenerateMarkupForAssembly(typeof(Uno.Material.MaterialResources))]
-[assembly: GenerateMarkupForAssembly(typeof(Uno.Themes.BaseTheme))]
 [assembly: AssemblyMetadata("IsTrimmable", "True")]
