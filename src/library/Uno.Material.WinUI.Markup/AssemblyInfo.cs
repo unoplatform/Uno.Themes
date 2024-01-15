7de54487ce6ca8b4b917e0e74a69d543e1229375
Pedro Jesus <pedrojesus.cefet@gmail.com>
2024-01-15 15:20:45 -0300

    chore: adds Uno.Theme as reference and generate markup for it

--- src/library/Uno.Material.WinUI.Markup/AssemblyInfo.cs
+++ src/library/Uno.Material.WinUI.Markup/AssemblyInfo.cs
@@ -2,4 +2,5 @@
 using Uno.Extensions.Markup.Generator;
 
 [assembly: GenerateMarkupForAssembly(typeof(Uno.Material.MaterialResources))]
+[assembly: GenerateMarkupForAssembly(typeof(Uno.Themes.BaseTheme))]
 [assembly: AssemblyMetadata("IsTrimmable", "True")]
