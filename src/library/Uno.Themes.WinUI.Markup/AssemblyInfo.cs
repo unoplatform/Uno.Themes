d97ce63da25bf16a8268a1c85e5d0934f28992ca
Pedro Jesus <pedrojesus.cefet@gmail.com>
2024-01-18 12:08:44 -0300

    chore: forcing Markup package on Uno.Themes

--- src/library/Uno.Themes.WinUI.Markup/AssemblyInfo.cs
+++ src/library/Uno.Themes.WinUI.Markup/AssemblyInfo.cs
@@ -4,4 +4,3 @@ using Uno.Extensions.Markup.Internals;
 
 [assembly: AssemblyMetadata("IsTrimmable", "True")]
 [assembly: ResourceDefinitionClass(typeof(Uno.Themes.Markup.Theme))]
-[assembly: GenerateMarkupForAssembly(typeof(Uno.Themes.BaseTheme))]
