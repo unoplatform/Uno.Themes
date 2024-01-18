3298afa6b010ca01220abeaaff1830999b8c6131
Pedro Jesus <pedrojesus.cefet@gmail.com>
2024-01-18 13:31:36 -0300

    fix: Fix Markup generator on Themes.WinUI.Markup

--- src/library/Uno.Themes.WinUI.Markup/AssemblyInfo.cs
+++ src/library/Uno.Themes.WinUI.Markup/AssemblyInfo.cs
@@ -4,3 +4,4 @@ using Uno.Extensions.Markup.Internals;
 
 [assembly: AssemblyMetadata("IsTrimmable", "True")]
 [assembly: ResourceDefinitionClass(typeof(Uno.Themes.Markup.Theme))]
+[assembly: GenerateMarkupForAssembly(typeof(Uno.Themes.BaseTheme))]
