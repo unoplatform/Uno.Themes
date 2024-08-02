3639ddbf9a7bc478a0f8421c8502c6c7afb33626
eriklimakc <erik.lima@knowcode.app>
2024-08-02 16:59:46 +0100

    chore: Using special VisualStateGroups to manage animations

--- src/samples/UWP/Uno.Themes.Samples.Shared/Content/Controls/TextBoxSamplePage.xaml.cs
+++ src/samples/UWP/Uno.Themes.Samples.Shared/Content/Controls/TextBoxSamplePage.xaml.cs
@@ -1,4 +1,4 @@
-﻿namespace Uno.Themes.Samples.Content.Controls;
+namespace Uno.Themes.Samples.Content.Controls;
 
 [SamplePage(SampleCategory.Controls, "TextBox", Description = "This control allows users to input a textual value.", DocumentationLink = "https://docs.microsoft.com/en-us/uwp/api/windows.ui.xaml.controls.textbox")]
 public sealed partial class TextBoxSamplePage : Page
@@ -6,5 +6,41 @@ public sealed partial class TextBoxSamplePage : Page
 	public TextBoxSamplePage()
 	{
 		this.InitializeComponent();
+	}
+
+	private void AddHeader(object sender, RoutedEventArgs e)
+	{
+		txtMyTb.Header = "Header";
+		txtMyTb1.Header = "Header";
+	}
+
+	private void RemoveHeader(object sender, RoutedEventArgs e)
+	{
+		txtMyTb.Header = null;
+		txtMyTb1.Header = null;
+	}
+
+	private void AddPlaceholder(object sender, RoutedEventArgs e)
+	{
+		txtMyTb.PlaceholderText = "Placeholder";
+		txtMyTb1.PlaceholderText = "Placeholder";
+	}
+
+	private void RemovePlaceholder(object sender, RoutedEventArgs e)
+	{
+		txtMyTb.PlaceholderText = null;
+		txtMyTb1.PlaceholderText = null;
+	}
+
+	private void AddText(object sender, RoutedEventArgs e)
+	{
+		txtMyTb.Text = "Text";
+		txtMyTb1.Text = "Text";
+	}
+
+	private void RemoveText(object sender, RoutedEventArgs e)
+	{
+		txtMyTb.Text = null;
+		txtMyTb1.Text = null;
 	}
 }
