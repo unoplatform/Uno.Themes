f71d18abd584760db00d2e4bd831e5f43a85d5c7
eriklimakc <erik.lima@knowcode.app>
2024-08-08 14:13:12 +0100

    chore: Adjust docs and remove test code

--- src/samples/UWP/Uno.Themes.Samples.Shared/Content/Controls/TextBoxSamplePage.xaml.cs
+++ src/samples/UWP/Uno.Themes.Samples.Shared/Content/Controls/TextBoxSamplePage.xaml.cs
@@ -7,40 +7,4 @@ public sealed partial class TextBoxSamplePage : Page
 	{
 		this.InitializeComponent();
 	}
-
-	private void AddHeader(object sender, RoutedEventArgs e)
-	{
-		txtMyTb.Header = "Header";
-		txtMyTb1.Header = "Header";
-	}
-
-	private void RemoveHeader(object sender, RoutedEventArgs e)
-	{
-		txtMyTb.Header = null;
-		txtMyTb1.Header = null;
-	}
-
-	private void AddPlaceholder(object sender, RoutedEventArgs e)
-	{
-		txtMyTb.PlaceholderText = "Placeholder";
-		txtMyTb1.PlaceholderText = "Placeholder";
-	}
-
-	private void RemovePlaceholder(object sender, RoutedEventArgs e)
-	{
-		txtMyTb.PlaceholderText = null;
-		txtMyTb1.PlaceholderText = null;
-	}
-
-	private void AddText(object sender, RoutedEventArgs e)
-	{
-		txtMyTb.Text = "Text";
-		txtMyTb1.Text = "Text";
-	}
-
-	private void RemoveText(object sender, RoutedEventArgs e)
-	{
-		txtMyTb.Text = null;
-		txtMyTb1.Text = null;
-	}
 }
