57c630d47adb36746e8c727e1ca6e5a71459a2e0
Matthew Rajala <matthew.rajala@polymtl.ca>
2025-05-20 16:07:48 -0400

    chore: WIP add debug click method

--- src/samples/UWP/Uno.Themes.Samples.Shared/Content/Controls/ButtonSamplePage.xaml.cs
+++ src/samples/UWP/Uno.Themes.Samples.Shared/Content/Controls/ButtonSamplePage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Uno.Themes.Samples.Content.Controls;
+using System.Threading.Tasks;
+
+namespace Uno.Themes.Samples.Content.Controls;
 
 [SamplePage(SampleCategory.Controls, "Button", Description = "A button is used to interpret a user click or tap interaction. They're often bound to commands.", DocumentationLink = "https://docs.microsoft.com/en-us/windows/uwp/design/controls-and-patterns/buttons")]
 public sealed partial class ButtonSamplePage : Page
@@ -7,4 +9,13 @@ public sealed partial class ButtonSamplePage : Page
 	{
 		this.InitializeComponent();
 	}
+
+	private async void Button_Click(object sender, RoutedEventArgs e)
+	{
+		if (sender is Button b)
+		{
+			b.IsEnabled = false;
+			var debug = 1;
+		}
+    }
 }
