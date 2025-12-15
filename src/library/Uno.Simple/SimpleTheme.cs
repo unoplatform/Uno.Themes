04de73a76bb3794e740e9fc724a23513b15fe2a2
Steve Bilogan <steve.bilogan@gmail.com>
2025-12-15 13:31:52 -0500

    chore: initial simple import

---
+++ src/library/Uno.Simple/SimpleTheme.cs
@@ -0,0 +1,43 @@
+using System;
+using Uno.Themes;
+
+
+#if WinUI
+using Microsoft.UI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Media;
+#else
+using Windows.UI.Xaml;
+#endif
+
+namespace Uno.Simple;
+
+/// <summary>
+/// Simple Theme resources including colors, fonts, layout values, and styles
+/// </summary>
+public class SimpleTheme : BaseTheme
+{
+	public SimpleTheme()
+	{ }
+
+	public SimpleTheme(ResourceDictionary colorOverride = null, ResourceDictionary fontOverride = null)
+		: base(colorOverride, fontOverride)
+	{ }
+
+	protected override ResourceDictionary GenerateSpecificResources()
+	{
+		var mergedPages = new ResourceDictionary { Source = new Uri(SimpleConstants.ResourcePaths.Version2.MergedPages) };
+
+		var fonts = new ResourceDictionary { Source = new Uri(SimpleConstants.ResourcePaths.Common.Fonts) };
+
+		if (FontOverrideDictionary is { } fontOverride)
+		{
+			fonts.SafeMerge(fontOverride);
+		}
+
+		mergedPages.MergedDictionaries.Add(fonts);
+		return mergedPages;
+	}
+}
