240f6df49a984adb9a8ee87b5538e92dd5174dbd
Kunal22shah <kunalshah606@gmail.com>
2026-01-12 15:41:25 -0500

    chore: PR comments

--- src/library/Uno.Simple/SimpleConstants.cs
+++ src/library/Uno.Simple/SimpleConstants.cs
@@ -2,7 +2,7 @@ namespace Uno.Simple;
 
 internal static class SimpleConstants
 {
-	public static string PackageName =
+	public static readonly string PackageName =
 #if WinUI
 		"Uno.Simple.WinUI";
 #else
@@ -12,13 +12,13 @@ internal static class SimpleConstants
 	public static class ResourcePaths
 	{
 		public static string ColorPalette = $"ms-appx:///{PackageName}/Styles/Application/ColorPalette.xaml";
-		public static string SimpleColors = $"ms-appx:///{PackageName}/Styles/Application/SimpleColors.xaml";
+		public static readonly string SimpleColors = $"ms-appx:///{PackageName}/Styles/Application/SimpleColors.xaml";
 		public static string Thickness = $"ms-appx:///{PackageName}/Styles/Application/Thickness.xaml";
-		public static string MergedPages = $"ms-appx:///{PackageName}/Generated/mergedpages.xaml";
+		public static readonly string MergedPages = $"ms-appx:///{PackageName}/Generated/mergedpages.xaml";
 		
 		public static class Common
 		{
-			public static string Fonts = $"ms-appx:///{PackageName}/Styles/Application/Common/Fonts.xaml";
+			public static readonly string Fonts = $"ms-appx:///{PackageName}/Styles/Application/Common/Fonts.xaml";
 		}
 	}
 }
