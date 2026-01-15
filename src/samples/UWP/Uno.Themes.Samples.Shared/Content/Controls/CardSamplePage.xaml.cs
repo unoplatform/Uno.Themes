b91e8e14c9889689df5b6154883838a2b27d8581
Kunal22shah <kunalshah606@gmail.com>
2026-01-15 13:18:50 -0500

    feat: simple design system library

---
+++ src/samples/UWP/Uno.Themes.Samples.Shared/Content/Controls/CardSamplePage.xaml.cs
@@ -0,0 +1,10 @@
+namespace Uno.Themes.Samples.Content.Controls;
+
+[SamplePage(SampleCategory.Controls, "Card", Description = "Card styles for grouping related content and actions in a visually contained unit.", DocumentationLink = "https://platform.uno/docs/articles/external/uno.toolkit.ui/doc/controls/CardAndCardContentControl.html")]
+public sealed partial class CardSamplePage : Page
+{
+	public CardSamplePage()
+	{
+		this.InitializeComponent();
+	}
+}
