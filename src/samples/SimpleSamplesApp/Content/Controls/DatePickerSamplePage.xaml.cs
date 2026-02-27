92f1509e2a3d544c17e7d875490fb0d0e6996e2a
Kunal Shah <37922021+Kunal22shah@users.noreply.github.com>
2026-02-27 11:12:10 -0500

    chore: align sds styles with figma specs (#1617)

---
+++ src/samples/SimpleSamplesApp/Content/Controls/DatePickerSamplePage.xaml.cs
@@ -0,0 +1,12 @@
+using Uno.Themes.Simple.Samples.Entities;
+
+namespace Uno.Themes.Simple.Samples.Content.Controls;
+
+[SamplePage(SampleCategory.Controls, "DatePicker", Symbol = Symbol.Calendar)]
+public sealed partial class DatePickerSamplePage : Page
+{
+    public DatePickerSamplePage()
+    {
+        this.InitializeComponent();
+    }
+}
---
+++ src/samples/SimpleSamplesApp/Content/Controls/ListViewSamplePage.xaml.cs
@@ -0,0 +1,12 @@
+using Uno.Themes.Simple.Samples.Entities;
+
+namespace Uno.Themes.Simple.Samples.Content.Controls;
+
+[SamplePage(SampleCategory.Controls, "ListView", Symbol = Symbol.List)]
+public sealed partial class ListViewSamplePage : Page
+{
+    public ListViewSamplePage()
+    {
+        this.InitializeComponent();
+    }
+}
